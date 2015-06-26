a5f0f3e62a236d6ed0ffbe789cda06192032357a
Xiaocan Li <phyxiaolee@gmail.com>
2015-06-26 17:08:17 -0600

    Add a new module to calculate compressional heating.

---
+++ src/modules/compression.f90
@@ -0,0 +1,7 @@
+!*******************************************************************************
+! Module dealing with compressional heating.
+!*******************************************************************************
+module compression
+    implicit none
+    private
+end module compression
