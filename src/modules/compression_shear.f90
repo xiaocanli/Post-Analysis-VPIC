2421c3c716eadc7ad2affcfc0c0e01be077dcf1b
Xiaocan Li <phyxiaolee@gmail.com>
2015-07-29 09:36:06 -0600

    Add routines to calculate div_udot_ptensor

--- src/modules/compression_shear.f90
+++ src/modules/compression_shear.f90
@@ -9,11 +9,15 @@ module compression_shear
     public pdiv_u, pshear, udot_div_ptensor, div_u, bbsigma
     public init_compression_shear, free_compression_shear, &
            calc_compression_shear, save_compression_shear, &
-           save_tot_compression_shear
+           save_tot_compression_shear, init_div_udot_ptensor, &
+           free_div_udot_ptensor, calc_div_udot_ptensor, &
+           save_div_udot_ptensor, save_tot_div_udot_ptensor
 
     real(fp), allocatable, dimension(:, :, :) :: pdiv_u, pshear
     real(fp), allocatable, dimension(:, :, :) :: udot_div_ptensor
     real(fp), allocatable, dimension(:, :, :) :: div_u, bbsigma
+    real(fp), allocatable, dimension(:, :, :) :: udot_ptensor_x, &
+            udot_ptensor_y, udot_ptensor_z, div_udot_ptensor
 
     contains
 
@@ -25,6 +29,7 @@ module compression_shear
         call init_compression
         call init_shear
         call init_udot_div_ptensor
+        call init_div_udot_ptensor
     end subroutine init_compression_shear
 
     !---------------------------------------------------------------------------
@@ -59,6 +64,21 @@ module compression_shear
     end subroutine init_udot_div_ptensor
 
     !---------------------------------------------------------------------------
+    ! Initialize div_udot_ptensor and the 3 components of udot_ptensor.
+    !---------------------------------------------------------------------------
+    subroutine init_div_udot_ptensor
+        implicit none
+        allocate(div_udot_ptensor(htg%nx, htg%ny, htg%nz))
+        allocate(udot_ptensor_x(htg%nx, htg%ny, htg%nz))
+        allocate(udot_ptensor_y(htg%nx, htg%ny, htg%nz))
+        allocate(udot_ptensor_z(htg%nx, htg%ny, htg%nz))
+        div_udot_ptensor = 0.0
+        udot_ptensor_x = 0.0
+        udot_ptensor_y = 0.0
+        udot_ptensor_z = 0.0
+    end subroutine init_div_udot_ptensor
+
+    !---------------------------------------------------------------------------
     ! Free the data arrays.
     !---------------------------------------------------------------------------
     subroutine free_compression_shear
@@ -66,6 +86,7 @@ module compression_shear
         call free_compression
         call free_shear
         call free_udot_div_ptensor
+        call free_div_udot_ptensor
     end subroutine free_compression_shear
 
     !---------------------------------------------------------------------------
@@ -91,6 +112,15 @@ module compression_shear
         implicit none
         deallocate(udot_div_ptensor)
     end subroutine free_udot_div_ptensor
+
+    !---------------------------------------------------------------------------
+    ! Free div_udot_ptensor and the 3 components of udot_ptensor.
+    !---------------------------------------------------------------------------
+    subroutine free_div_udot_ptensor
+        implicit none
+        deallocate(div_udot_ptensor)
+        deallocate(udot_ptensor_x, udot_ptensor_y, udot_ptensor_z)
+    end subroutine free_div_udot_ptensor
 
     !---------------------------------------------------------------------------
     ! Calculate the divergence of u.
@@ -225,7 +255,43 @@ module compression_shear
         call calc_compression
         call calc_shear
         call calc_udot_div_ptensor
+        call calc_div_udot_ptensor
     end subroutine calc_compression_shear
+
+    !---------------------------------------------------------------------------
+    ! Calculate div_udot_ptensor.
+    !---------------------------------------------------------------------------
+    subroutine calc_div_udot_ptensor
+        use pic_fields, only: ux, uy, uz, pxx, pxy, pxz, pyy, pyz, pzz
+        use neighbors_module, only: ixl, iyl, izl, ixh, iyh, izh, idx, idy, idz
+        implicit none
+        integer :: nx, ny, nz, ix, iy, iz
+
+        udot_ptensor_x = ux * pxx + uy * pxy + uz * pxz
+        udot_ptensor_y = ux * pxy + uy * pyy + uz * pyz
+        udot_ptensor_z = ux * pxz + uy * pyz + uz * pzz
+
+        nx = htg%nx
+        ny = htg%ny
+        nz = htg%nz
+
+        do ix = 1, nx
+            div_udot_ptensor(ix, :, :) = (udot_ptensor_x(ixh(ix), :, :) - &
+                    udot_ptensor_x(ixl(ix), :, :)) * idx(ix)
+        enddo
+
+        do iy = 1, ny
+            div_udot_ptensor(:, iy, :) = div_udot_ptensor(:, iy, :) + &
+                    (udot_ptensor_y(:, iyh(iy), :) - &
+                     udot_ptensor_y(:, iyl(iy), :)) * idy(iy)
+        enddo
+
+        do iz = 1, nz
+            div_udot_ptensor(:, :, iz) = div_udot_ptensor(:, :, iz) + &
+                    (udot_ptensor_z(:, :, izh(iz)) - &
+                     udot_ptensor_z(:, :, izl(iz))) * idz(iz)
+        enddo
+    end subroutine calc_div_udot_ptensor
 
     !---------------------------------------------------------------------------
     ! Save div_u.
@@ -350,7 +416,26 @@ module compression_shear
         call save_compression(ct)
         call save_shear(ct)
         call save_udot_div_ptensor(ct)
+        call save_div_udot_ptensor(ct)
     end subroutine save_compression_shear
+
+    !---------------------------------------------------------------------------
+    ! Save div_udot_ptensor.
+    ! Input:
+    !   ct: current time frame.
+    !---------------------------------------------------------------------------
+    subroutine save_div_udot_ptensor(ct)
+        use mpi_module
+        use constants, only: fp
+        use mpi_io_fields, only: save_field
+        implicit none
+        integer, intent(in) :: ct
+        if (myid == master) then
+            print*, 'Saving div_udot_ptensor', ct
+        endif
+
+        call save_field(div_udot_ptensor, 'div_udot_ptensor', ct)
+    end subroutine save_div_udot_ptensor
 
     !---------------------------------------------------------------------------
     ! Save the total of the compressional terms.
@@ -455,7 +540,7 @@ module compression_shear
 
         call get_average_and_total(udot_div_ptensor, avg, udot_div_ptensor_tot)
         if (myid == master) then
-            fname = 'data/shear'//ibtag//'_'//species//'.gda'
+            fname = 'data/udot_div_ptensor'//ibtag//'_'//species//'.gda'
             open(unit=51, file=trim(adjustl(fname)), access='stream',&
                  status='unknown', form='unformatted', action='write')
             output_record = ct - tp1 + 1
@@ -476,6 +561,44 @@ module compression_shear
         call save_tot_compression(ct)
         call save_tot_shear(ct)
         call save_tot_udot_div_ptensor(ct)
+        call save_tot_div_udot_ptensor(ct)
     end subroutine save_tot_compression_shear
+
+    !---------------------------------------------------------------------------
+    ! Save the total of div_udot_ptensor.
+    ! Input:
+    !   ct: current time frame.
+    !---------------------------------------------------------------------------
+    subroutine save_tot_div_udot_ptensor(ct)
+        use mpi_module
+        use constants, only: fp
+        use particle_info, only: ibtag, species
+        use statistics, only: get_average_and_total
+        use parameters, only: tp1
+        implicit none
+        integer, intent(in) :: ct
+        real(fp) :: div_udot_ptensor_tot, avg
+        character(len=100) :: fname
+        integer :: current_pos, output_record
+        logical :: dir_e
+    
+        if (myid == master) then
+            inquire(file='./data/.', exist=dir_e)
+            if (.not. dir_e) then
+                call system('mkdir ./data')
+            endif
+        endif
+
+        call get_average_and_total(div_udot_ptensor, avg, div_udot_ptensor_tot)
+        if (myid == master) then
+            fname = 'data/div_udot_ptensor'//ibtag//'_'//species//'.gda'
+            open(unit=51, file=trim(adjustl(fname)), access='stream',&
+                 status='unknown', form='unformatted', action='write')
+            output_record = ct - tp1 + 1
+            current_pos = sizeof(fp) * (output_record-1) + 1
+            write(51, pos=current_pos) div_udot_ptensor_tot
+            close(51)
+        endif
+    end subroutine save_tot_div_udot_ptensor
 
 end module compression_shear
