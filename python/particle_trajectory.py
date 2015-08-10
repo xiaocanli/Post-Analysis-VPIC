9c2dd02ab9e515ffd7db0d47eb84c4db51684f9a
Xiaocan Li <phyxiaolee@gmail.com>
2015-08-09 19:52:26 -0600

    Updated analysis for trajectory.

--- python/particle_trajectory.py
+++ python/particle_trajectory.py
@@ -69,7 +69,7 @@ def read_traj_data(filename):
             bx=traj_data[10,:], by=traj_data[11,:], bz=traj_data[12,:])
     return ptl_traj
 
-def plot_traj(filename, pic_info, species, iptl):
+def plot_traj(filename, pic_info, species, iptl, mint, maxt):
     """Plot particle trajectory information.
 
     Args:
@@ -77,6 +77,7 @@ def plot_traj(filename, pic_info, species, iptl):
         pic_info: namedtuple for the PIC simulation information.
         species: particle species. 'e' for electron. 'i' for ion.
         iptl: particle ID.
+        mint, maxt: minimum and maximum time for plotting.
     """
     ptl_traj = read_traj_data(filename)
     gama = np.sqrt(ptl_traj.ux**2 + ptl_traj.uy**2 + ptl_traj.uz**2 + 1.0)
@@ -149,10 +150,12 @@ def plot_traj(filename, pic_info, species, iptl):
     ax1.set_xlim([xmin, xmax])
     ax3.set_xlim([xmin, xmax])
 
-    dir = '../img_traj_' + species + '/'
+    if not os.path.isdir('../img/'):
+        os.makedirs('../img/')
+    dir = '../img/img_traj_' + species + '/'
     if not os.path.isdir(dir):
         os.makedirs(dir)
-    fname = dir + 'traj_' + species + '_' + str(iptl).zfill(4) + '_1.eps'
+    fname = dir + 'traj_' + species + '_' + str(iptl).zfill(4) + '_1.jpg'
     fig.savefig(fname)
 
     height = 15.0
@@ -161,11 +164,16 @@ def plot_traj(filename, pic_info, species, iptl):
     xs, ys = 0.10, 0.98-h1
     gap = 0.025
 
+    dt = t[1] - t[0]
+    ct1 = int(mint / dt)
+    ct2 = int(maxt / dt)
+
     ax1 = fig.add_axes([xs, ys, w1, h1])
-    p1 = ax1.plot(t, gama-1.0, color='k')
+    p1 = ax1.plot(t[ct1:ct2], gama[ct1:ct2]-1.0, color='k')
     ax1.set_ylabel(r'$E/m_ic^2$', fontdict=font)
     ax1.tick_params(labelsize=20)
     ax1.tick_params(axis='x', labelbottom='off')
+    ax1.plot([mint, maxt], [0, 0], '--', color='k')
     ax1.text(0.4, -0.07, r'$x$', color='red', fontsize=32, 
             bbox=dict(facecolor='none', alpha=1.0, edgecolor='none', pad=10.0),
             horizontalalignment='center', verticalalignment='center',
@@ -181,9 +189,10 @@ def plot_traj(filename, pic_info, species, iptl):
 
     ys -= h1 + gap
     ax2 = fig.add_axes([xs, ys, w1, h1])
-    p21 = ax2.plot(t, ptl_traj.ux, color='r', label=r'u_x')
-    p22 = ax2.plot(t, ptl_traj.uy, color='g', label=r'u_y')
-    p23 = ax2.plot(t, ptl_traj.uz, color='b', label=r'u_z')
+    p21 = ax2.plot(t[ct1:ct2], ptl_traj.ux[ct1:ct2], color='r', label=r'u_x')
+    p22 = ax2.plot(t[ct1:ct2], ptl_traj.uy[ct1:ct2], color='g', label=r'u_y')
+    p23 = ax2.plot(t[ct1:ct2], ptl_traj.uz[ct1:ct2], color='b', label=r'u_z')
+    ax2.plot([mint, maxt], [0, 0], '--', color='k')
     ax2.set_ylabel(r'$u_x, u_y, u_z$', fontdict=font)
     ax2.tick_params(labelsize=20)
     ax2.tick_params(axis='x', labelbottom='off')
@@ -193,48 +202,110 @@ def plot_traj(filename, pic_info, species, iptl):
     ys -= h1 + gap
     ax31 = fig.add_axes([xs, ys, w1, h1])
     ex = signal.medfilt(ptl_traj.ex, kernel_size=(kernel))
-    p31 = ax31.plot(t, ex, color='r', label=r'E_x')
+    p31 = ax31.plot(t[ct1:ct2], ex[ct1:ct2], color='r', label=r'E_x')
     ax31.set_ylabel(r'$E_x$', fontdict=font)
     ax31.tick_params(labelsize=20)
     ax31.tick_params(axis='x', labelbottom='off')
-    ax31.plot([0, tmax], [0, 0], '--', color='k')
+    ax31.plot([mint, maxt], [0, 0], '--', color='k')
 
     ys -= h1 + gap
     ax32 = fig.add_axes([xs, ys, w1, h1])
     ey = signal.medfilt(ptl_traj.ey, kernel_size=(kernel))
-    p32 = ax32.plot(t, ey, color='g', label=r'E_y')
+    p32 = ax32.plot(t[ct1:ct2], ey[ct1:ct2], color='g', label=r'E_y')
     ax32.set_ylabel(r'$E_y$', fontdict=font)
     ax32.tick_params(labelsize=20)
     ax32.tick_params(axis='x', labelbottom='off')
-    ax32.plot([0, tmax], [0, 0], '--', color='k')
+    ax32.plot([mint, maxt], [0, 0], '--', color='k')
 
     ys -= h1 + gap
     ax33 = fig.add_axes([xs, ys, w1, h1])
     ez = signal.medfilt(ptl_traj.ez, kernel_size=(kernel))
-    p33 = ax33.plot(t, ez, color='b', label=r'E_z')
+    p33 = ax33.plot(t[ct1:ct2], ez[ct1:ct2], color='b', label=r'E_z')
     ax33.set_ylabel(r'$E_z$', fontdict=font)
     ax33.tick_params(labelsize=20)
     ax33.tick_params(axis='x', labelbottom='off')
-    ax33.plot([0, tmax], [0, 0], '--', color='k')
+    ax33.plot([mint, maxt], [0, 0], '--', color='k')
 
     ys -= h1 + gap
     ax4 = fig.add_axes([xs, ys, w1, h1])
-    p41 = ax4.plot(t, ptl_traj.bx, color='r', label=r'B_x')
-    p42 = ax4.plot(t, ptl_traj.by, color='g', label=r'B_y')
-    p43 = ax4.plot(t, ptl_traj.bz, color='b', label=r'B_z')
+    p41 = ax4.plot(t[ct1:ct2], ptl_traj.bx[ct1:ct2], color='r', label=r'B_x')
+    p42 = ax4.plot(t[ct1:ct2], ptl_traj.by[ct1:ct2], color='g', label=r'B_y')
+    p43 = ax4.plot(t[ct1:ct2], ptl_traj.bz[ct1:ct2], color='b', label=r'B_z')
     ax4.set_xlabel(r'$t\Omega_{ci}$', fontdict=font)
     ax4.set_ylabel(r'$B_x, B_y, B_z$', fontdict=font)
     ax4.tick_params(labelsize=20)
-    xmax = 800
-    ax1.set_xlim([0, xmax])
-    ax2.set_xlim([0, xmax])
-    ax31.set_xlim([0, xmax])
-    ax32.set_xlim([0, xmax])
-    ax33.set_xlim([0, xmax])
-    ax4.set_xlim([0, xmax])
+    ax4.plot([mint, maxt], [0, 0], '--', color='k')
+    ax1.set_xlim([mint, maxt])
+    ax2.set_xlim([mint, maxt])
+    ax31.set_xlim([mint, maxt])
+    ax32.set_xlim([mint, maxt])
+    ax33.set_xlim([mint, maxt])
+    ax4.set_xlim([mint, maxt])
 
     fname = dir + 'traj_' + species + '_' + str(iptl).zfill(4) + '_2.eps'
     fig.savefig(fname)
+
+    height = 6.0
+    fig = plt.figure(figsize=[width, height])
+    w1, h1 = 0.88, 0.4
+    xs, ys = 0.10, 0.97-h1
+    gap = 0.05
+
+    dt = t[1] - t[0]
+    ct1 = int(mint / dt)
+    ct2 = int(maxt / dt)
+
+    if species == 'e':
+        charge = -1.0
+    else:
+        charge = 1.0
+    jdote_x = ptl_traj.ux * ptl_traj.ex * charge / gama
+    jdote_y = ptl_traj.uy * ptl_traj.ey * charge / gama
+    jdote_z = ptl_traj.uz * ptl_traj.ez * charge / gama
+    nt, = t.shape
+    dt = np.zeros(nt)
+    dt[0:nt-1] = np.diff(t)
+    jdote_x_cum = np.cumsum(jdote_x) * dt
+    jdote_y_cum = np.cumsum(jdote_y) * dt
+    jdote_z_cum = np.cumsum(jdote_z) * dt
+    jdote_tot_cum = jdote_x_cum + jdote_y_cum + jdote_z_cum
+    
+    ax1 = fig.add_axes([xs, ys, w1, h1])
+    p1 = ax1.plot(t[ct1:ct2], jdote_x[ct1:ct2], color='r')
+    p2 = ax1.plot(t[ct1:ct2], jdote_y[ct1:ct2], color='g')
+    p3 = ax1.plot(t[ct1:ct2], jdote_z[ct1:ct2], color='b')
+    ax1.tick_params(labelsize=20)
+    ax1.tick_params(axis='x', labelbottom='off')
+    ax1.plot([mint, maxt], [0, 0], '--', color='k')
+    if species == 'e':
+        charge = '-e'
+    else:
+        charge = 'e'
+    text1 = r'$' + charge + 'u_x' + 'E_x' + '$'
+    ax1.text(0.1, 0.1, text1, color='red', fontsize=32, 
+            bbox=dict(facecolor='none', alpha=1.0, edgecolor='none', pad=10.0),
+            horizontalalignment='center', verticalalignment='center',
+            transform = ax1.transAxes)
+    text2 = r'$' + charge + 'u_y' + 'E_y' + '$'
+    ax1.text(0.2, 0.1, text2, color='green', fontsize=32, 
+            bbox=dict(facecolor='none', alpha=1.0, edgecolor='none', pad=10.0),
+            horizontalalignment='center', verticalalignment='center',
+            transform = ax1.transAxes)
+    text3 = r'$' + charge + 'u_z' + 'E_z' + '$'
+    ax1.text(0.3, 0.1, text3, color='blue', fontsize=32, 
+            bbox=dict(facecolor='none', alpha=1.0, edgecolor='none', pad=10.0),
+            horizontalalignment='center', verticalalignment='center',
+            transform = ax1.transAxes)
+
+    ys -= h1 + gap
+    ax2 = fig.add_axes([xs, ys, w1, h1])
+    p1 = ax2.plot(t[ct1:ct2], jdote_x_cum[ct1:ct2], color='r')
+    p2 = ax2.plot(t[ct1:ct2], jdote_y_cum[ct1:ct2], color='g')
+    p3 = ax2.plot(t[ct1:ct2], jdote_z_cum[ct1:ct2], color='b')
+    p4 = ax2.plot(t[ct1:ct2], jdote_tot_cum[ct1:ct2], color='k')
+    ax2.tick_params(labelsize=20)
+    ax2.set_xlabel(r'$t\Omega_{ci}$', fontdict=font)
+    ax2.plot([mint, maxt], [0, 0], '--', color='k')
 
     plt.show()
 
@@ -244,5 +315,5 @@ if __name__ == "__main__":
     ntp = pic_info.ntp
     vthe = pic_info.vthe
     fnames_e, fnames_i = get_file_names()
-    iptl = 316
-    plot_traj(fnames_i[iptl], pic_info, 'i', iptl)
+    iptl = 209
+    plot_traj(fnames_i[iptl], pic_info, 'i', iptl, 0, 400)
