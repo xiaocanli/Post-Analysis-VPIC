5da8d06f6195b3b92d106aa301426e55cd2a7a06
Xiaocan Li <phyxiaolee@gmail.com>
2015-09-18 15:36:44 -0600

    Update sorter.sh

--- vpic-sorter/sorter.sh
+++ vpic-sorter/sorter.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# export filepath=/scratch3/guofan/open3d-h5tracer/tracer
-export filepath=/net/scratch1/guofan/share/ultra-sigma/sigma1e4-mime100-4000-track/tracer
+export filepath=/scratch3/guofan/open3d-h5tracer/tracer
+# export filepath=/net/scratch1/guofan/share/ultra-sigma/sigma1e4-mime100-4000-track/tracer
 
 for D in `find $filepath ! -path $filepath -type d`
 do
     # echo $D
     arr=( $(echo $D | awk -F "." '{print $2}') )
     tstep=${arr[0]}
-    echo $filepath/T.$tstep/electron_tracer.h5p
-    mpirun -np 128 ./h5group-sorter -f $filepath/T.$tstep/electron_tracer.h5p -g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_electron_tracer.h5p -o $filepath/T.$tstep/electron_tracer_sorted.h5p -k 7 -a attribute
+    if [ $tstep -eq 102 ]
+    then
+        echo $filepath/T.$tstep/electron_tracer_sorted.h5p
+        mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/electron_tracer.h5p \
+        -o $filepath/T.$tstep/electron_tracer_sorted.h5p \
+        -g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_electron_tracer.h5p \
+        -k 7 -a attribute
+    fi
 done
