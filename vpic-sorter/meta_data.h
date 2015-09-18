bd2e73f79db0a27328ef938f8a2a6f56d489982a
Xiaocan Li <phyxiaolee@gmail.com>
2015-09-18 09:35:15 -0600

    Update meta_data.c to calculate particle positions

--- vpic-sorter/meta_data.h
+++ vpic-sorter/meta_data.h
@@ -1,1 +1,3 @@
-void read_vpic_meta_data(char *filename, char *group_name);
+void calc_particle_positions(int mpi_rank, hsize_t my_offset, int row_size,
+        int max_type_size, hsize_t my_data_size, char* filename_meta,
+        char *group_name, char *package_data);
