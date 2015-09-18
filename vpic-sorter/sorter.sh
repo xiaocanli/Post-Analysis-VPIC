d0127db0d008bc8548b584ccc0d86b20b6e9a902
Xiaocan Li <phyxiaolee@gmail.com>
2015-09-18 16:21:14 -0600

    Update sorter.sh

--- vpic-sorter/sorter.sh
+++ vpic-sorter/sorter.sh
@@ -8,12 +8,12 @@ do
     # echo $D
     arr=( $(echo $D | awk -F "." '{print $2}') )
     tstep=${arr[0]}
-    if [ $tstep -eq 102 ]
-    then
-        echo $filepath/T.$tstep/electron_tracer_sorted.h5p
-        mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/electron_tracer.h5p \
-        -o $filepath/T.$tstep/electron_tracer_sorted.h5p \
-        -g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_electron_tracer.h5p \
-        -k 7 -a attribute
-    fi
+    # if [ $tstep -eq 102 ]
+    # then
+    echo $filepath/T.$tstep/electron_tracer_sorted.h5p
+    mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/electron_tracer.h5p \
+    -o $filepath/T.$tstep/electron_tracer_sorted.h5p \
+    -g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_electron_tracer.h5p \
+    -k 7 -a attribute
+    # fi
 done
