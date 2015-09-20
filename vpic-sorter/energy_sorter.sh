b443f50bc41855d475c2a6bb0504665a6aa98b85
Xiaocan Li <phyxiaolee@gmail.com>
2015-09-20 09:52:06 -0600

    Change energy_sorter.sh for both electron and ion

--- vpic-sorter/energy_sorter.sh
+++ vpic-sorter/energy_sorter.sh
@@ -4,9 +4,10 @@
 export filepath=/net/scratch1/guofan/share/ultra-sigma/sigma1e4-mime100-4000-track/tracer
 
 export tstep=26236
+export particle=ion
 
 # echo $filepath/T.$tstep/electron_tracer_sorted.h5p
-mpirun -np 64 ./h5group-sorter -f $filepath/T.$tstep/electron_tracer.h5p \
--o $filepath/T.$tstep/electron_tracer_energy_sorted.h5p \
--g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_electron_tracer.h5p \
+mpirun -np 64 ./h5group-sorter -f $filepath/T.$tstep/${particle}_tracer.h5p \
+-o $filepath/T.$tstep/${particle}_tracer_energy_sorted.h5p \
+-g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_${particle}_tracer.h5p \
 -k 8 -a attribute
