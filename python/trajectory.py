dc0be084e74099e18b33ccff33def48e838ed1be
Xiaocan Li <phyxiaolee@gmail.com>
2015-09-19 23:41:41 -0600

    Update trajectory diagnostic user-interface

--- python/trajectory.py
+++ python/trajectory.py
@@ -34,7 +34,7 @@ particles = collections.namedtuple("particles",
         ["x", "y", "z", "ux", "uy", "uz"])
 
 class Viewer2d(object):
-    def __init__(self, file, nptl, x, y, fdata):
+    def __init__(self, file, nptl, x, y, fdata, init_ft, var_field, var_name):
         """
         Shows a given array in a 2d-viewer.
         Input: z, an 2d array.
@@ -45,20 +45,28 @@ class Viewer2d(object):
         self.fdata = fdata
         self.file = file
         self.nptl = nptl
+        self.ct = init_ft
+        self.iptl = 0
         self.particle_tags = []
         for item in self.file:
             self.particle_tags.append(item)
-        group = file[self.particle_tags[1]]
+        group = file[self.particle_tags[self.iptl]]
         dset_ux = group['Ux']
         self.sz, = dset_ux.shape
         self.particle_info = ParticleInfo(self.sz)
         self.ptl = self.particle_info.get_particle_info(group)
+        self.pic_info = pic_information.get_pic_info('../../')
+        self.smime = math.sqrt(self.pic_info.mime)
+        self.px = self.ptl.x / self.smime
+        self.py = self.ptl.y / self.smime
+        self.pz = self.ptl.z / self.smime
         self.gama = np.sqrt(self.ptl.ux**2 + self.ptl.uy**2 + self.ptl.uz**2 + 1.0)
 
         self.fig = plt.figure(figsize=(10, 10))
 
         self.pxz_axis = self.fig.add_axes([0.1, 0.73, 0.8, 0.25])
         vmax = min(abs(np.min(self.fdata)), abs(np.max(self.fdata)))
+        vmax *= 0.2
         self.im1 = self.pxz_axis.imshow(self.fdata, cmap=plt.cm.seismic,
                 extent=[np.min(self.x), np.max(self.x), np.min(self.y), np.max(self.y)],
                 aspect='auto', origin='lower',
@@ -68,14 +76,15 @@ class Viewer2d(object):
         self.cax = divider.append_axes("right", size="2%", pad=0.05)
         self.cbar = self.fig.colorbar(self.im1, cax=self.cax)
         self.cbar.ax.tick_params(labelsize=16)
+        self.cbar.ax.set_ylabel(var_name, fontdict=font, fontsize=24)
         self.pxz_axis.tick_params(labelsize=16)
         self.pxz_axis.set_xlabel(r'$x/d_i$', fontdict=font, fontsize=20)
         self.pxz_axis.set_ylabel(r'$z/d_i$', fontdict=font, fontsize=20)
         self.pxz_axis.autoscale(1,'both',1)
 
         # xz plot
-        self.pxz, = self.pxz_axis.plot(self.ptl.x, self.ptl.z, linewidth=2,
-                color='k')
+        self.pxz, = self.pxz_axis.plot(self.px, self.pz, linewidth=2,
+                color='k', marker='.', markersize=1, linestyle='')
 
         # Energy plot
         self.ene_axis = self.fig.add_axes([0.1, 0.46, 0.35, 0.2])
@@ -87,7 +96,7 @@ class Viewer2d(object):
         # x-energy
         self.xe_axis = self.fig.add_axes([0.6, 0.46, 0.35, 0.2])
         self.xe_axis.tick_params(labelsize=16)
-        self.pxe, = self.xe_axis.plot(self.ptl.x, self.gama - 1.0,
+        self.pxe, = self.xe_axis.plot(self.px, self.gama - 1.0,
                 linewidth=2, color='r')
         self.xe_axis.set_ylabel(r'$\gamma - 1$', fontdict=font, fontsize=20)
         self.xe_axis.set_xlabel(r'$x$', fontdict=font, fontsize=20)
@@ -95,7 +104,7 @@ class Viewer2d(object):
         # y-energy
         self.ye_axis = self.fig.add_axes([0.1, 0.2, 0.35, 0.2])
         self.ye_axis.tick_params(labelsize=16)
-        self.pye, = self.ye_axis.plot(self.ptl.y, self.gama - 1.0,
+        self.pye, = self.ye_axis.plot(self.py, self.gama - 1.0,
                 linewidth=2, color='g')
         self.ye_axis.set_ylabel(r'$\gamma - 1$', fontdict=font, fontsize=20)
         self.ye_axis.set_xlabel(r'$y$', fontdict=font, fontsize=20)
@@ -103,53 +112,98 @@ class Viewer2d(object):
         # z-energy
         self.ze_axis = self.fig.add_axes([0.6, 0.2, 0.35, 0.2])
         self.ze_axis.tick_params(labelsize=16)
-        self.pze, = self.ze_axis.plot(self.ptl.z, self.gama - 1.0,
+        self.pze, = self.ze_axis.plot(self.pz, self.gama - 1.0,
                 linewidth=2, color='b')
         self.ze_axis.set_ylabel(r'$\gamma - 1$', fontdict=font, fontsize=20)
         self.ze_axis.set_xlabel(r'$z$', fontdict=font, fontsize=20)
 
         # Slider to choose particle
-        pic_info = pic_information.get_pic_info('../../')
         self.sliderax = plt.axes([0.1, 0.1, 0.8, 0.03])
         particle_list = np.arange(1, self.nptl)
         self.slider = DiscreteSlider(self.sliderax, 'Particle', 1, self.nptl,\
-                allowed_vals=particle_list, valinit=particle_list[0])
+                allowed_vals=particle_list, valinit=particle_list[self.iptl])
         self.slider.on_changed(self.update_particle)
 
         # Slider to choose time frames for fields
         self.field_sliderax = plt.axes([0.1, 0.05, 0.8, 0.03])
-        tframes = np.arange(1, pic_info.ntf)
+        tframes = np.arange(1, self.pic_info.ntf)
         self.field_slider = DiscreteSlider(self.field_sliderax, 'Field', 1,
-                pic_info.ntf, allowed_vals=tframes, valinit=tframes[19])
+                self.pic_info.ntf, allowed_vals=tframes, valinit=tframes[self.ct-1])
         self.field_slider.on_changed(self.update_field)
 
         self._widgets=[self.slider, self.field_slider]
+        self.save_figure()
 
     def update_particle(self, val):
-        iptl = self.slider.val
-        group = file[self.particle_tags[iptl]]
+        self.iptl = self.slider.val - 1
+        group = file[self.particle_tags[self.iptl]]
         self.ptl = self.particle_info.get_particle_info(group)
+        self.px = self.ptl.x / self.smime
+        self.py = self.ptl.y / self.smime
+        self.pz = self.ptl.z / self.smime
         self.gama = np.sqrt(self.ptl.ux**2 + self.ptl.uy**2 + self.ptl.uz**2 + 1.0)
-        self.pxz.set_xdata(self.ptl.x)
-        self.pxz.set_ydata(self.ptl.z)
+        emax = np.max(self.gama - 1.0)
+        xmin = np.min(self.px)
+        xmax = np.max(self.px)
+        ymin = np.min(self.py)
+        ymax = np.max(self.py)
+        zmin = np.min(self.pz)
+        zmax = np.max(self.pz)
+        self.ene_axis.set_ylim([0, emax*1.1])
+        self.xe_axis.set_ylim([0, emax*1.1])
+        self.ye_axis.set_ylim([0, emax*1.1])
+        self.ze_axis.set_ylim([0, emax*1.1])
+        self.xe_axis.set_xlim([xmin*0.9, xmax*1.1])
+        if ymin < 0:
+            yl = ymin * 1.1
+        else:
+            yl = ymin * 0.9
+
+        if ymax < 0:
+            yr = ymax * 0.9
+        else:
+            yr = ymax * 1.1
+
+        if zmin < 0:
+            zl = zmin * 1.1
+        else:
+            zl = zmin * 0.9
+
+        if zmax < 0:
+            zr = zmax * 0.9
+        else:
+            zr = zmax * 1.1
+        self.ye_axis.set_xlim([yl, yr])
+        self.ze_axis.set_xlim([zl, zr])
+        self.pxz.set_xdata(self.px)
+        self.pxz.set_ydata(self.pz)
         self.pene.set_ydata(self.gama - 1.0)
-        self.pxe.set_xdata(self.ptl.x)
-        self.pye.set_xdata(self.ptl.y)
-        self.pze.set_xdata(self.ptl.z)
+        self.pxe.set_xdata(self.px)
+        self.pye.set_xdata(self.py)
+        self.pze.set_xdata(self.pz)
         self.pxe.set_ydata(self.gama - 1.0)
         self.pye.set_ydata(self.gama - 1.0)
         self.pze.set_ydata(self.gama - 1.0)
         self.fig.canvas.draw_idle()
+        self.save_figure()
 
     def update_field(self, val):
-        ct = self.field_slider.val
-        print ct
-        pic_info = pic_information.get_pic_info('../../')
-        kwargs = {"current_time":ct, "xl":0, "xr":330, "zb":-40, "zt":40}
-        fname = "../../data/jy.gda"
-        x, z, self.fdata = read_2d_fields(pic_info, fname, **kwargs) 
+        self.ct = self.field_slider.val
+        kwargs = {"current_time":self.ct, "xl":0, "xr":400, "zb":-100, "zt":100}
+        fname = '../../data/' + var_field + '.gda'
+        x, z, self.fdata = read_2d_fields(self.pic_info, fname, **kwargs) 
         self.im1.set_data(self.fdata)
         self.fig.canvas.draw_idle()
+
+    def save_figure(self):
+        if not os.path.isdir('../img/'):
+            os.makedirs('../img/')
+        if not os.path.isdir('../img/img_traj/'):
+            os.makedirs('../img/img_traj/')
+        fname = '../img/img_traj/traj_' + str(self.iptl) + '_' + \
+                str(self.ct).zfill(3) + '.jpg'
+        self.fig.savefig(fname, dpi=200)
+
 
 class DiscreteSlider(Slider):
     """A matplotlib slider widget with discrete steps."""
@@ -216,12 +270,17 @@ class ParticleInfo(object):
   
 if __name__ == "__main__":
     pic_info = pic_information.get_pic_info('../../')
-    fname = '/scratch3/xiaocanli/open3d-h5tracer/pic_analysis/vpic-sorter/data/test.h5p'
+    filepath = '/net/scratch1/guofan/share/ultra-sigma/'
+    filepath += 'sigma1e4-mime100-4000-track/pic_analysis/vpic-sorter/data/'
+    fname = filepath + 'electrons.h5p'
     file = h5py.File(fname, 'r')
     ngroups = len(file)
-    kwargs = {"current_time":20, "xl":0, "xr":330, "zb":-40, "zt":40}
-    fname = "../../data/jy.gda"
+    init_ft = 40
+    var_field = 'jy'
+    var_name = '$j_y$'
+    kwargs = {"current_time":init_ft, "xl":0, "xr":400, "zb":-100, "zt":100}
+    fname = '../../data/' + var_field + '.gda'
     x, z, data = read_2d_fields(pic_info, fname, **kwargs) 
-    fig_v = Viewer2d(file, ngroups, x, z, data)
+    fig_v = Viewer2d(file, ngroups, x, z, data, init_ft, var_field, var_name)
     plt.show()
     file.close()
