be2eb46ea0279f84f9e025fd21796f047f5d0cf0
Xiaocan Li <phyxiaolee@gmail.com>
2015-09-19 23:40:19 -0600

    Have sorter.sh work for both electron and ion

--- vpic-sorter/sorter.sh
+++ vpic-sorter/sorter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export filepath=/scratch3/guofan/open3d-h5tracer/tracer
-# export filepath=/net/scratch1/guofan/share/ultra-sigma/sigma1e4-mime100-4000-track/tracer
+# export filepath=/scratch3/guofan/open3d-h5tracer/tracer
+export filepath=/net/scratch1/guofan/share/ultra-sigma/sigma1e4-mime100-4000-track/tracer
+export particle=ion
 
 for D in `find $filepath ! -path $filepath -type d`
 do
@@ -10,10 +11,10 @@ do
     tstep=${arr[0]}
     # if [ $tstep -eq 102 ]
     # then
-    echo $filepath/T.$tstep/electron_tracer_sorted.h5p
-    mpirun -np 16 ./h5group-sorter -f $filepath/T.$tstep/electron_tracer.h5p \
-    -o $filepath/T.$tstep/electron_tracer_sorted.h5p \
-    -g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_electron_tracer.h5p \
+    echo $filepath/T.$tstep/${particle}_tracer_sorted.h5p
+    mpirun -np 64 ./h5group-sorter -f $filepath/T.$tstep/${particle}_tracer.h5p \
+    -o $filepath/T.$tstep/${particle}_tracer_sorted.h5p \
+    -g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_${particle}_tracer.h5p \
     -k 7 -a attribute
     # fi
 done
