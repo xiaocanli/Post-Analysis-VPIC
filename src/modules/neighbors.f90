8679c727168a5ac2541f9ca4ec1fcf3aecac9751
Xiaocan Li <phyxiaolee@gmail.com>
2015-09-21 13:52:15 -0600

    Delete the comment part

--- src/modules/neighbors.f90
+++ src/modules/neighbors.f90
@@ -18,34 +18,6 @@ module neighbors_module
 
     contains
 
-    ! !---------------------------------------------------------------------------
-    ! ! Decide the two adjoint points.
-    ! ! Input:
-    ! !   ntot: total number of points in this dimension.
-    ! !   cindex: index of current point.
-    ! ! Output:
-    ! !   index1: index of left/bottom point. 
-    ! !   index2: index of the right/top point.
-    ! !---------------------------------------------------------------------------
-    ! subroutine adjoint_points(ntot, cindex, index1, index2)
-    !     implicit none
-    !     integer, intent(in) :: ntot, cindex
-    !     integer, intent(out) :: index1, index2
-    !     if (ntot == 1) then
-    !         index1 = 1
-    !         index2 = 1
-    !     else if (cindex == 1) then
-    !         index1 = 1
-    !         index2 = 2
-    !     else if (cindex == ntot) then
-    !         index1 = ntot-1
-    !         index2 = ntot
-    !     else
-    !         index1 = cindex - 1
-    !         index2 = cindex + 1
-    !     endif
-    ! end subroutine adjoint_points
-
     !---------------------------------------------------------------------------
     ! Initialize the indices of the neighbors and the inverse of the distance
     ! between them.
