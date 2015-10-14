bb0f3abbb6020a6167de46ccf3cfc54df5288dfd
Xiaocan Li <phyxiaolee@gmail.com>
2015-10-14 14:48:07 -0600

    Plot reconnection rate for different comparison

--- python/reconnection_rate.py
+++ python/reconnection_rate.py
@@ -16,6 +16,7 @@ import struct
 import collections
 import pic_information
 from contour_plots import read_2d_fields, plot_2d_contour
+import palettable
 
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 mpl.rc('text', usetex=True)
@@ -59,6 +60,7 @@ def calc_reconnection_rate(base_dir):
     b0 = pic_info.b0
     va = dtwce * math.sqrt(1.0/mime) / dtwpe
     reconnection_rate /= b0 * va
+    reconnection_rate[-1] = reconnection_rate[-2]
     tfields = pic_info.tfields
 
     return (tfields, reconnection_rate)
@@ -101,75 +103,115 @@ def plot_reconnection_rate(base_dir):
 def calc_multi_reconnection_rate():
     """Calculate reconnection rate for multiple runs
     """
-    # base_dir = '/net/scratch2/xiaocanli/mime25-sigma01-beta02-200-100/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime25_beta02.dat')
+    base_dir = '/net/scratch2/xiaocanli/mime25-sigma01-beta02-200-100/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime25_beta02.dat')
 
-    # base_dir = '/net/scratch2/xiaocanli/mime25-sigma033-beta006-200-100/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime25_beta007.dat')
+    base_dir = '/net/scratch2/xiaocanli/mime25-sigma033-beta006-200-100/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime25_beta007.dat')
 
-    # base_dir = '/scratch3/xiaocanli/sigma1-mime25-beta001/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime25_beta002.dat')
+    base_dir = '/scratch3/xiaocanli/sigma1-mime25-beta001/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime25_beta002.dat')
 
-    # base_dir = '/scratch3/xiaocanli/sigma1-mime25-beta0003-npc200/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime25_beta0007.dat')
+    base_dir = '/scratch3/xiaocanli/sigma1-mime25-beta0003-npc200/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime25_beta0007.dat')
 
-    # base_dir = '/scratch3/xiaocanli/sigma1-mime100-beta001-mustang/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime100_beta002.dat')
+    base_dir = '/scratch3/xiaocanli/sigma1-mime100-beta001-mustang/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime100_beta002.dat')
 
-    # base_dir = '/scratch3/xiaocanli/mime25-guide0-beta001-200-100/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime25_beta002_sigma01.dat')
+    base_dir = '/scratch3/xiaocanli/mime25-guide0-beta001-200-100/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime25_beta002_sigma01.dat')
 
-    # base_dir = '/scratch3/xiaocanli/mime25-guide0-beta001-200-100-sigma033/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime25_beta002_sigma033.dat')
+    base_dir = '/scratch3/xiaocanli/mime25-guide0-beta001-200-100-sigma033/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime25_beta002_sigma033.dat')
 
-    # base_dir = '/net/scratch2/xiaocanli/mime25-sigma1-beta002-200-100-noperturb/'
-    # t, rate = calc_reconnection_rate(base_dir)
-    # save_reconnection_rate(t, rate, 'rate_mime25_beta002_noperturb.dat')
+    base_dir = '/net/scratch2/xiaocanli/mime25-sigma1-beta002-200-100-noperturb/'
+    t, rate = calc_reconnection_rate(base_dir)
+    save_reconnection_rate(t, rate, 'rate_mime25_beta002_noperturb.dat')
 
 
 def plot_multi_reconnection_rate():
     """Calculate reconnection rate for multiple runs
     """
     path = '../data/rate/'
-    fname = path + 'rate_mime25_beta02.dat'
-    tf1, rate1 = np.genfromtxt(fname)
-    fname = path + 'rate_mime25_beta007.dat'
-    tf2, rate2 = np.genfromtxt(fname)
     fname = path + 'rate_mime25_beta002.dat'
+    tf1, rate1 = np.genfromtxt(fname)
+    fname = path + 'rate_mime25_beta002_sigma033.dat'
+    tf2, rate2 = np.genfromtxt(fname)
+    fname = path + 'rate_mime25_beta002_sigma01.dat'
     tf3, rate3 = np.genfromtxt(fname)
-    fname = path + 'rate_mime25_beta0007.dat'
+    fname = path + 'rate_mime25_beta002_noperturb.dat'
     tf4, rate4 = np.genfromtxt(fname)
     fname = path + 'rate_mime100_beta002.dat'
     tf5, rate5 = np.genfromtxt(fname)
-    fname = path + 'rate_mime25_beta002_sigma01.dat'
+    fname = path + 'rate_mime25_beta0007.dat'
     tf6, rate6 = np.genfromtxt(fname)
-    fname = path + 'rate_mime25_beta002_sigma033.dat'
+    fname = path + 'rate_mime25_beta007.dat'
     tf7, rate7 = np.genfromtxt(fname)
-    fname = path + 'rate_mime25_beta002_noperturb.dat'
+    fname = path + 'rate_mime25_beta02.dat'
     tf8, rate8 = np.genfromtxt(fname)
 
+    if not os.path.isdir('../img/'):
+        os.makedirs('../img/')
+    dir = '../img/rate/'
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
     fig = plt.figure(figsize=[7, 5])
     ax = fig.add_axes([0.18, 0.15, 0.78, 0.8])
-    # ax.plot(tf1, rate1, linewidth=2)
-    # ax.plot(tf2, rate2, linewidth=2)
-    ax.plot(tf3, rate3, linewidth=2)
-    ax.plot(tf4, rate4, linewidth=2)
-    # ax.plot(tf5, rate5, linewidth=2)
-    ax.plot(tf6, rate6, linewidth=2)
-    ax.plot(tf7, rate7, linewidth=2)
-    ax.plot(tf8, rate8, linewidth=2)
+    colors = palettable.colorbrewer.qualitative.Set1_9.mpl_colors
+    ax.set_color_cycle(colors)
+    ax.plot(tf6, rate6, linewidth=2, label='R6')
+    ax.plot(tf8, rate8, linewidth=2, label='R8')
+    # ax.plot(tf7, rate7, linewidth=2, label='R7')
     ax.set_xlabel(r'$t\Omega_{ci}$', fontdict=font, fontsize=24)
     ax.set_ylabel(r'$E_R$', fontdict=font, fontsize=24)
     ax.tick_params(labelsize=20)
     ax.set_xlim([0, 1200])
     ax.set_ylim([0, 0.12])
+    ax.legend(loc=1, prop={'size':20}, ncol=1,
+            shadow=False, fancybox=False, frameon=False)
+    fname = dir + 'rate_low_density.eps'
+    fig.savefig(fname)
+
+    fig = plt.figure(figsize=[7, 5])
+    ax = fig.add_axes([0.18, 0.15, 0.78, 0.8])
+    ax.set_color_cycle(colors)
+    ax.plot(tf1, rate1, linewidth=2, label='R1')
+    ax.plot(tf2, rate2, linewidth=2, label='R2')
+    ax.plot(tf3, rate3, linewidth=2, label='R3')
+    # ax.plot(tf4, rate4, linewidth=2, label='R4')
+    # ax.plot(tf5, rate5, linewidth=2, label='R5')
+    ax.set_xlabel(r'$t\Omega_{ci}$', fontdict=font, fontsize=24)
+    ax.set_ylabel(r'$E_R$', fontdict=font, fontsize=24)
+    ax.tick_params(labelsize=20)
+    ax.set_xlim([0, 1200])
+    ax.set_ylim([0, 0.12])
+    ax.legend(loc=1, prop={'size':20}, ncol=1,
+            shadow=False, fancybox=False, frameon=False)
+    fname = dir + 'rate_low_temp.eps'
+    fig.savefig(fname)
+
+    fig = plt.figure(figsize=[7, 5])
+    ax = fig.add_axes([0.18, 0.15, 0.78, 0.8])
+    ax.set_color_cycle(colors)
+    ax.plot(tf1, rate1, linewidth=2, label='R1')
+    ax.plot(tf5, rate5, linewidth=2, label='R5')
+    ax.set_xlabel(r'$t\Omega_{ci}$', fontdict=font, fontsize=24)
+    ax.set_ylabel(r'$E_R$', fontdict=font, fontsize=24)
+    ax.tick_params(labelsize=20)
+    ax.set_xlim([0, 1200])
+    ax.set_ylim([0, 0.12])
+    ax.legend(loc=1, prop={'size':20}, ncol=1,
+            shadow=False, fancybox=False, frameon=False)
+    fname = dir + 'rate_mass.eps'
+    fig.savefig(fname)
+
     plt.show()
 
 
