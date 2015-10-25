43c07ef9574b1d1ebd315a165c6439f66d70a984
Xiaocan Li <phyxiaolee@gmail.com>
2015-10-25 16:02:41 -0600

    Load or calculate parameters before calculation
    1. Calculate the velocity interval for velocity distribution
    2. Load magnetic field for parallel and perpendicular velocity

--- src/programs/vdistribution.f90
+++ src/programs/vdistribution.f90
@@ -4,18 +4,20 @@
 program vdistribution
     use mpi_module
     use path_info, only: get_file_paths
-    use picinfo, only: read_domain, broadcast_pic_info
+    use picinfo, only: read_domain, broadcast_pic_info, domain
     use particle_frames, only: get_particle_frames, nt, tinterval
     use spectrum_config, only: read_spectrum_config, set_spatial_range_de, &
             calc_pic_mpi_ids, tframe, init_pic_mpi_ranks, free_pic_mpi_ranks, &
-            calc_pic_mpi_ranks
+            calc_pic_mpi_ranks, calc_velocity_interval
     use velocity_distribution, only: init_velocity_bins, free_velocity_bins, &
             init_vdist_2d, set_vdist_2d_zero, free_vdist_2d, init_vdist_1d, &
             set_vdist_1d_zero, free_vdist_1d, calc_vdist_2d, calc_vdist_1d
     use parameters, only: get_start_end_time_points, get_inductive_flag, &
             get_relativistic_flag
+    use magnetic_field, only: init_magnetic_fields, free_magnetic_fields, &
+            read_magnetic_fields
     implicit none
-    integer :: ct
+    integer :: ct, ct_field, ratio_particle_field
     ! Initialize Message Passing
     call MPI_INIT(ierr)
     call MPI_COMM_RANK(MPI_COMM_WORLD, myid, ierr)
@@ -36,6 +38,7 @@ program vdistribution
     call get_inductive_flag
     call get_relativistic_flag
     call read_spectrum_config
+    call calc_velocity_interval
     call set_spatial_range_de
     call calc_pic_mpi_ids
     call init_pic_mpi_ranks
@@ -45,10 +48,19 @@ program vdistribution
     call init_vdist_2d
     call init_vdist_1d
 
+    call init_magnetic_fields
+
+    ! Ratio of particle output interval to fields output interval
+    ratio_particle_field = domain%Particle_interval / domain%fields_interval
+    ct_field = ratio_particle_field * tframe
+    call read_magnetic_fields(ct_field)
+
     call calc_vdist_2d(tframe, 'e')
     ! call set_vdist_2d_zero
     call calc_vdist_1d(tframe, 'e')
     ! call set_vdist_1d_zero
+
+    call free_magnetic_fields
 
     call free_vdist_1d
     call free_vdist_2d
