271cc7ab40b8a75763e2aaa4b4f413d52132d71b
Xiaocan Li <phyxiaolee@gmail.com>
2015-10-26 03:59:56 -0600

    Update particle_distribution

--- python/particle_distribution.py
+++ python/particle_distribution.py
@@ -9,12 +9,17 @@ from matplotlib.colors import LogNorm
 from matplotlib import rc
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
+import scipy
+from scipy import interpolate, signal
 import math
 import os.path
 import struct
 import collections
 import pic_information
 import color_maps as cm
+import colormap.colormaps as cmaps
+import subprocess
+from spectrum_fitting import get_energy_distribution
 
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 mpl.rc('text', usetex=True)
@@ -186,40 +191,66 @@ def get_particle_distribution(base_directory, tindex, corners, mpi_ranks):
                 hist_xy += vhist_xy
                 hist_xz += vhist_xz
                 hist_yz += vhist_yz
-    vmax = np.max([np.max(hist_xy), np.max(hist_xz), np.max(hist_yz)])
-    vmax = math.log10(vmax)
-    xs, ys = 0.04, 0.1
-    w1, h1 = 0.28, 0.84
-    gap = 0.05
-    fig = plt.figure(figsize=(12, 4))
-    ax1 = fig.add_axes([xs, ys, w1, h1])
-    p1 = ax1.imshow(np.log10(hist_xy), cmap=plt.cm.jet,
-            extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
-            aspect='auto', origin='lower',
-            vmin = 0.0, vmax = vmax)
-            # interpolation='bicubic')
-    xs += w1 + 0.05
-    ax2 = fig.add_axes([xs, ys, w1, h1])
-    p2 = ax2.imshow(np.log10(hist_xz), cmap=plt.cm.jet,
-            extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
-            aspect='auto', origin='lower',
-            vmin = 0.0, vmax = vmax)
-            # interpolation='bicubic')
-    xs += w1 + 0.05
-    ax3 = fig.add_axes([xs, ys, w1, h1])
-    p3 = ax3.imshow(np.log10(hist_yz), cmap=plt.cm.jet,
-            extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
-            aspect='auto', origin='lower',
-            vmin = 0.0, vmax = vmax)
-            # interpolation='bicubic')
-    p1.set_cmap(plt.cm.get_cmap('idl40'))
-    p2.set_cmap(plt.cm.get_cmap('idl40'))
-    p3.set_cmap(plt.cm.get_cmap('idl40'))
-    plt.show()
+    # uold = np.linspace(-1.0, 1.0, 64)
+    # u1, u2 = np.meshgrid(uold, uold)
+    # ng = 3
+    # kernel = np.ones((ng,ng)) / float(ng*ng)
+    # hist_xy = signal.convolve2d(hist_xy, kernel, 'same')
+    # hist_xz = signal.convolve2d(hist_xz, kernel, 'same')
+    # hist_yz = signal.convolve2d(hist_yz, kernel, 'same')
+    # fxy = interpolate.interp2d(u1, u2, np.log10(hist_xy+0.5), kind='cubic')
+    # fxz = interpolate.interp2d(u1, u2, np.log10(hist_xz+0.5), kind='cubic')
+    # fyz = interpolate.interp2d(u1, u2, np.log10(hist_yz+0.5), kind='cubic')
+    # unew = np.linspace(-1.0, 1.0, 200)
+    # fxy_new = fxy(unew, unew)
+    # fxz_new = fxz(unew, unew)
+    # fyz_new = fyz(unew, unew)
+
+    # fxy = fxy_new
+    # fxz = fxz_new
+    # fyz = fyz_new
+
+    # vmax = np.max([np.max(hist_xy), np.max(hist_xz), np.max(hist_yz)])
+    # vmax = math.log10(vmax)
+    # xs, ys = 0.08, 0.17
+    # w1, h1 = 0.24, 0.72
+    # gap = 0.08
+    # fig = plt.figure(figsize=(12, 4))
+    # ax1 = fig.add_axes([xs, ys, w1, h1])
+    # p1 = ax1.imshow(fxy_new, cmap=plt.cm.jet,
+    #         extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
+    #         aspect='auto', origin='lower',
+    #         vmin = 0.0, vmax = vmax)
+    #         # interpolation='bicubic')
+    # ax1.set_xlabel(r'$u_x$', fontdict=font, fontsize=20)
+    # ax1.set_ylabel(r'$u_y$', fontdict=font, fontsize=20)
+    # ax1.tick_params(labelsize=16)
+    # xs += w1 + gap
+    # ax2 = fig.add_axes([xs, ys, w1, h1])
+    # p2 = ax2.imshow(fxz_new, cmap=plt.cm.jet,
+    #         extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
+    #         aspect='auto', origin='lower',
+    #         vmin = 0.0, vmax = vmax)
+    # ax2.set_xlabel(r'$u_x$', fontdict=font, fontsize=20)
+    # ax2.set_ylabel(r'$u_z$', fontdict=font, fontsize=20)
+    # ax2.tick_params(labelsize=16)
+    # xs += w1 + gap
+    # ax3 = fig.add_axes([xs, ys, w1, h1])
+    # p3 = ax3.imshow(fyz_new, cmap=plt.cm.jet,
+    #         extent=[np.min(x), np.max(x), np.min(y), np.max(y)],
+    #         aspect='auto', origin='lower',
+    #         vmin = 0.0, vmax = vmax)
+    #         # interpolation='bicubic')
+    # ax3.set_xlabel(r'$u_y$', fontdict=font, fontsize=20)
+    # ax3.set_ylabel(r'$u_z$', fontdict=font, fontsize=20)
+    # ax3.tick_params(labelsize=16)
+    # p1.set_cmap(plt.cm.get_cmap('hot'))
+    # p2.set_cmap(plt.cm.get_cmap('hot'))
+    # p3.set_cmap(plt.cm.get_cmap('hot'))
+    # plt.show()
 
 
-
-def set_mpi_ranks(pic_info, center=np.zeros(3), sizes=np.ones(3)*4):
+def set_mpi_ranks(pic_info, center=np.zeros(3), sizes=np.ones(3)*400):
     """Set MPI ranks for getting particle data
 
     Args:
@@ -283,6 +314,186 @@ def set_mpi_ranks(pic_info, center=np.zeros(3), sizes=np.ones(3)*4):
     mpi_ranks = [[ixs, ixe], [iys, iye], [izs, ize]]
     return (corners, mpi_ranks)
 
+def generate_spectrum_vdist_config(fname, **kwargs):
+    """Generate spectrum and velocity distribution configuration
+
+    Args:
+        fname: filename of the configuration file.
+    """
+    with open(fname, 'w+') as f:
+        center = kwargs['center']
+        sizes = kwargs['sizes']
+        f.write('***** Configuration file for velocity distribution *****\n')
+        f.write('\n')
+        f.write('nbins = 600\n')
+        f.write('emax = 100.0\n')
+        f.write('emin = 0.0001\n')
+        f.write('xc/de = %6.2f\n' % center[0])
+        f.write('yc/de = %6.2f\n' % center[1])
+        f.write('zc/de = %6.2f\n' % center[2])
+        f.write('xsize = %d\n' % sizes[0])
+        f.write('ysize = %d\n' % sizes[1])
+        f.write('zsize = %d\n' % sizes[2])
+        f.write('nbins_vdist = %d\n' % kwargs['nbins'])
+        f.write('vmax = %6.2f\n' % kwargs['vmax'])
+        f.write('vmin = %6.2f\n' % kwargs['vmin'])
+        f.write('tframe = %d\n' % kwargs['tframe'])
+        f.close()
+
+
+def get_spectrum_vdist(pic_info, dir='../',
+        config_name='config_files/vdist_config.dat', **kwargs):
+    """Get particle spectra and velocity distributions
+    """
+    fname = dir + config_name
+    generate_spectrum_vdist_config(fname, **kwargs)
+    cmd = './particle_spectrum_vdist_box ' + config_name
+    p1 = subprocess.Popen([cmd], cwd='../', shell=True)
+    # cmd = 'mpirun -np 16 ./particle_spectrum_vdist_box ' + config_name
+    # p1 = subprocess.Popen([cmd], cwd='../', stdout=subprocess.PIPE, shell=True)
+    p1.wait()
+
+
+def read_velocity_distribution(species, tframe, pic_info,
+        fpath='../vdistributions/'):
+    """Read velocity distribution from a file.
+
+    Args:
+        fpath: file path for the data.
+        species: particle species.
+        tframe: time frame.
+        pic_info: particle information namedtuple.
+    """
+    # 2D distributions
+    if species == 'e':
+        fname = fpath + 'vdist_2d-' + species + '.' + str(tframe)
+    else:
+        fname = fpath + 'vdist_2d-h.' + str(tframe)
+    f = open(fname, 'r')
+    center = np.zeros(3)
+    sizes = np.zeros(3)
+    offset = 0
+    center = np.memmap(f, dtype='float32', mode='r', 
+            offset=offset, shape=(3), order='C')
+    offset = 3 * 4
+    sizes = np.memmap(f, dtype='float32', mode='r', 
+            offset=offset, shape=(3), order='C')
+    offset += 3 * 4
+    vmin, vmax = np.memmap(f, dtype='float32', mode='c', 
+            offset=offset, shape=(2), order='C')
+    offset += 2 * 4
+    nbins, = np.memmap(f, dtype='int32', mode='r', 
+            offset=offset, shape=1, order='C')
+    offset += 4
+    vbins_short = np.zeros(nbins)
+    vbins_long = np.zeros(nbins*2)
+    vbins_short = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(nbins), order='C')
+    offset += 8 * nbins
+    vbins_long = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(2*nbins), order='C')
+    offset += 8 * nbins * 2
+    fvel_para_perp = np.zeros((nbins, 2*nbins))
+    fvel_xy = np.zeros((2*nbins, 2*nbins))
+    fvel_xz = np.zeros((2*nbins, 2*nbins))
+    fvel_yz = np.zeros((2*nbins, 2*nbins))
+    fvel_para_perp = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(nbins, 2*nbins), order='C')
+    offset += 8 * nbins * 2 * nbins
+    fvel_xy = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(2*nbins, 2*nbins), order='C')
+    offset += 8 * 2 * nbins * 2 * nbins
+    fvel_xz = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(2*nbins, 2*nbins), order='C')
+    offset += 8 * 2 * nbins * 2 * nbins
+    fvel_yz = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(2*nbins, 2*nbins), order='C')
+    f.close()
+
+    if species == 'e':
+        fname = fpath + 'vdist_1d-' + species + '.' + str(tframe)
+    else:
+        fname = fpath + 'vdist_1d-h.' + str(tframe)
+    f = open(fname, 'r')
+    # skip headers
+    offset = 9 * 4 + 8 * nbins * 3
+    fvel_para = np.zeros(2*nbins)
+    fvel_perp = np.zeros(nbins)
+    fvel_para = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(2*nbins), order='C')
+    offset += 8 * nbins * 2
+    fvel_perp = np.memmap(f, dtype='float64', mode='c', 
+            offset=offset, shape=(nbins), order='C')
+    f.close()
+
+    # Adjust the vbins. For ions, the actual saved variables is
+    # sqrt(m_i) * u
+    smime = math.sqrt(pic_info.mime)
+    if species == 'h':
+        vbins_short /= smime
+        vbins_long /= smime
+        vmin /= smime
+        vmax /= smime
+
+    # Add small number to the distributions to avoid zeros
+    # delta = vmin_2d * 0.1
+    delta = 0.01
+    fvel_para_perp += delta
+    fvel_xy += delta
+    fvel_xz += delta
+    fvel_yz += delta
+
+    # delta = vmin_1d * 0.1
+    fvel_para += delta
+    fvel_perp += delta
+    vmin_2d = min(np.min(fvel_para_perp[np.nonzero(fvel_para_perp)]),
+            np.min(fvel_xy[np.nonzero(fvel_xy)]),
+            np.min(fvel_xz[np.nonzero(fvel_xz)]),
+            np.min(fvel_yz[np.nonzero(fvel_yz)]))
+    vmax_2d = max(np.max(fvel_para_perp[np.nonzero(fvel_para_perp)]),
+            np.max(fvel_xy[np.nonzero(fvel_xy)]),
+            np.max(fvel_xz[np.nonzero(fvel_xz)]),
+            np.max(fvel_yz[np.nonzero(fvel_yz)]))
+    vmin_1d = min(np.min(fvel_para[np.nonzero(fvel_para)]),
+            np.min(fvel_perp[np.nonzero(fvel_perp)]))
+    vmax_1d = max(np.max(fvel_para[np.nonzero(fvel_para)]),
+            np.max(fvel_perp[np.nonzero(fvel_perp)]))
+
+    fvelocity = collections.namedtuple("fvelocity", 
+            ['species', 'tframe', 'center', 'sizes', 'vmin', 'vmax', 'nbins',
+             'vbins_short', 'vbins_long', 'fvel_para_perp', 'fvel_xy',
+             'fvel_xz', 'fvel_yz', 'fvel_para', 'fvel_perp', 'vmin_2d',
+             'vmax_2d', 'vmin_1d', 'vmax_1d'])
+
+    fvel = fvelocity(species=species, tframe=tframe, center=center,
+            sizes=sizes, vmin=vmin, vmax=vmax, nbins=nbins,
+            vbins_short=vbins_short, vbins_long=vbins_long,
+            fvel_para_perp=fvel_para_perp, fvel_xy=fvel_xy, fvel_xz=fvel_xz,
+            fvel_yz=fvel_yz, fvel_para=fvel_para, fvel_perp=fvel_perp,
+            vmin_2d=vmin_2d, vmax_2d=vmax_2d, vmin_1d=vmin_1d, vmax_1d=vmax_1d)
+    return fvel
+
+
+def read_energy_distribution(species, tframe, pic_info, fpath='../spectrum/'):
+    """Read particle energy spectrum from a file.
+
+    Args:
+        fpath: file path for the data.
+        species: particle species.
+        tframe: time frame.
+        pic_info: particle information namedtuple.
+    """
+    if species == 'e':
+        fname = fpath + 'spectrum-' + species + '.' + str(tframe)
+    else:
+        fname = fpath + 'spectrum-h.' + str(tframe)
+    ntot = pic_info.nx * pic_info.ny + pic_info.nz * pic_info.nppc
+    elin, flin, elog, flog = get_energy_distribution(fname, ntot)
+    fenergy = collections.namedtuple('fenergy',
+            ['species', 'elin', 'flin', 'elog', 'flog'])
+    fene = fenergy(species=species, elin=elin, flin=flin, elog=elog, flog=flog)
+    return fene
+
 
 if __name__ == "__main__":
     base_directory = '../../'
@@ -290,7 +501,18 @@ if __name__ == "__main__":
     ntp = pic_info.ntp
     vthe = pic_info.vthe
     particle_interval = pic_info.particle_interval
-    pos = [pic_info.lx_di/2, 0.0, 40.0]
+    pos = [pic_info.lx_di/10, 0.0, 2.0]
     corners, mpi_ranks = set_mpi_ranks(pic_info, pos)
-    ct = 1 * particle_interval
-    get_particle_distribution(base_directory, ct, corners, mpi_ranks)
+    ct = 5 * particle_interval
+    # get_particle_distribution(base_directory, ct, corners, mpi_ranks)
+    smime = math.sqrt(pic_info.mime)
+    lx_de = pic_info.lx_di * smime
+    center = [0.5*lx_de, 0, 0]
+    sizes = [50, 1, 8]
+    kwargs = {'center':center, 'sizes':sizes, 'nbins':64, 'vmax':2.0,
+            'vmin':0, 'tframe':10}
+    get_spectrum_vdist(pic_info, **kwargs)
+    # fpath = '../vdistributions/'
+    # fvel = read_velocity_distribution(fpath, 'e', 10)
+    # fpath = '../spectrum/'
+    # fene = read_energy_distribution(fpath, 'e', 10, pic_info)
