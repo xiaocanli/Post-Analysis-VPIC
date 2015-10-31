995ba787133f85039a25539354a677cf3a29468e
Xiaocan Li <phyxiaolee@gmail.com>
2015-10-31 15:02:47 -0600

    Change STRLEN to 64
    64 is enough

--- src/modules/kinds.f90
+++ src/modules/kinds.f90
@@ -29,7 +29,7 @@ module kinds
   integer(kind=4), public, parameter :: real_kind = 4
   integer(kind=4), public, parameter :: ptr_kind = 8
   
-  integer(kind=4), public, parameter :: STRLEN = 80
+  integer(kind=4), public, parameter :: STRLEN = 64
   integer(kind=4), public, parameter :: XSTRLEN = 256
   
 end module kinds
