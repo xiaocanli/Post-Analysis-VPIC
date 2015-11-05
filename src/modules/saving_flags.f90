fbdfcaf2c11c6800b7fccde2b58996e8162effdb
Xiaocan Li <phyxiaolee@gmail.com>
2015-11-04 20:31:26 -0700

    Set saving_flags for jdote due to different current

--- src/modules/saving_flags.f90
+++ src/modules/saving_flags.f90
@@ -8,7 +8,12 @@ module saving_flags
     public save_jcpara, save_jcperp, save_jmag, save_jgrad, save_jdiagm, &
            save_jpolar, save_jexb, save_jpara, save_jperp, save_jperp1, &
            save_jperp2, save_jqnvpara, save_jqnvperp, save_jagy, save_jtot, &
-           save_jdivv, save_pre
+           save_jdivv, save_pre, &
+           save_jcpara_dote, save_jcperp_dote, save_jmag_dote, save_jgrad_dote, &
+           save_jdiagm_dote, save_jpolar_dote, save_jexb_dote, save_jpara_dote, &
+           save_jperp_dote, save_jperp1_dote, save_jperp2_dote, &
+           save_jqnvpara_dote, save_jqnvperp_dote, save_jagy_dote, &
+           save_jtot_dote, save_jdivv_dote
     public get_saving_flags
 
     integer :: save_jcpara, save_jcperp, save_jmag
@@ -16,6 +21,11 @@ module saving_flags
     integer :: save_jexb, save_jpara, save_jperp
     integer :: save_jperp1, save_jperp2, save_jqnvpara
     integer :: save_jqnvperp, save_jagy, save_jtot, save_jdivv
+    integer :: save_jcpara_dote, save_jcperp_dote, save_jmag_dote
+    integer :: save_jgrad_dote, save_jdiagm_dote, save_jpolar_dote
+    integer :: save_jexb_dote, save_jpara_dote, save_jperp_dote
+    integer :: save_jperp1_dote, save_jperp2_dote, save_jqnvpara_dote
+    integer :: save_jqnvperp_dote, save_jagy_dote, save_jtot_dote, save_jdivv_dote
     integer :: save_pre
 
     contains
@@ -46,6 +56,22 @@ module saving_flags
         save_jtot = get_variable_int(fh, 'save_jtot', '=')
         save_jtot = get_variable_int(fh, 'save_jdivv', '=')
         save_pre = get_variable_int(fh, 'save_pre', '=')
+        save_jcpara_dote = get_variable_int(fh, 'save_jcpara_dote', '=')
+        save_jcperp_dote = get_variable_int(fh, 'save_jcperp_dote', '=')
+        save_jmag_dote = get_variable_int(fh, 'save_jmag_dote', '=')
+        save_jgrad_dote = get_variable_int(fh, 'save_jgrad_dote', '=')
+        save_jdiagm_dote = get_variable_int(fh, 'save_jdiagm_dote', '=')
+        save_jpolar_dote = get_variable_int(fh, 'save_jpolar_dote', '=')
+        save_jexb_dote = get_variable_int(fh, 'save_jexb_dote', '=')
+        save_jpara_dote = get_variable_int(fh, 'save_jpara_dote', '=')
+        save_jperp_dote = get_variable_int(fh, 'save_jperp_dote', '=')
+        save_jperp1_dote = get_variable_int(fh, 'save_jperp1_dote', '=')
+        save_jperp2_dote = get_variable_int(fh, 'save_jperp2_dote', '=')
+        save_jqnvpara_dote = get_variable_int(fh, 'save_jqnvpara_dote', '=')
+        save_jqnvperp_dote = get_variable_int(fh, 'save_jqnvperp_dote', '=')
+        save_jagy_dote = get_variable_int(fh, 'save_jagy_dote', '=')
+        save_jtot_dote = get_variable_int(fh, 'save_jtot_dote', '=')
+        save_jtot_dote = get_variable_int(fh, 'save_jdivv_dote', '=')
         close(fh)
     end subroutine get_saving_flags
 
