6799d9713615e98cace83b5925f76b69bff62783
Xiaocan Li <phyxiaolee@gmail.com>
2016-01-25 13:07:27 -0700

    Use \ for special character #

--- vpic-sorter/sorter.sh
+++ vpic-sorter/sorter.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # export filepath=/scratch3/guofan/open3d-h5tracer/tracer
-export filepath=/net/scratch1/guofan/share/ultra-sigma/sigma1e4-mime100-4000-track/tracer
-export particle=ion
+# export filepath=/net/scratch1/guofan/share/ultra-sigma/sigma1e4-mime100-4000-track/tracer
+export filepath=/net/scratch2/guofan/turbulent-sheet3D-mixing-trinity/tracer
+export particle=electron
 
 for D in `find $filepath ! -path $filepath -type d`
 do
     # echo $D
     arr=( $(echo $D | awk -F "." '{print $2}') )
     tstep=${arr[0]}
-    # if [ $tstep -eq 102 ]
+    # if [ $tstep -eq 1002 ]
     # then
     echo $filepath/T.$tstep/${particle}_tracer_sorted.h5p
     mpirun -np 64 ./h5group-sorter -f $filepath/T.$tstep/${particle}_tracer.h5p \
     -o $filepath/T.$tstep/${particle}_tracer_sorted.h5p \
-    -g /Step#$tstep -m $filepath/T.$tstep/grid_metadata_${particle}_tracer.h5p \
+    -g /Step\#$tstep -m $filepath/T.$tstep/grid_metadata_${particle}_tracer.h5p \
     -k 7 -a attribute
     # fi
 done
