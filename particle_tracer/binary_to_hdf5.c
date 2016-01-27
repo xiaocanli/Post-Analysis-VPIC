87225fb052f3f0947b2edce4f59b93d18cc86d1d
Xiaocan Li <phyxiaolee@gmail.com>
2016-01-26 20:26:00 -0700

    Initial commit for transferring binary data to HDF5

---
+++ particle_tracer/binary_to_hdf5.c
@@ -0,0 +1,21 @@
+/* 
+ *
+ * This transfers the particle trajectories in binary format to HDF5 format.
+ * 
+ */
+
+#include "stdlib.h"
+#include "hdf5.h"
+#include <unistd.h>
+#include <string.h>
+#include <float.h>
+#include <omp.h>
+#include <stdio.h>
+#include <math.h>
+
+/******************************************************************************
+ * Main program to transfer the data
+ ******************************************************************************/
+int main(int argc, char **argv){
+    return 0;
+}
---
+++ particle_tracer/module.sh
@@ -0,0 +1,5 @@
+#!/bin/tcsh
+
+module load friendly-testing
+module load gcc/4.8.2
+module load hdf5-serial/1.8.13
