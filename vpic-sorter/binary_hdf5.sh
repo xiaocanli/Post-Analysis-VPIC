80db62e8ed180f995723651b3939a92db87067a8
Xiaocan Li <phyxiaolee@gmail.com>
2016-02-17 07:57:20 -0700

    Update the program to transfer binary file to hdf5

--- vpic-sorter/binary_hdf5.sh
+++ vpic-sorter/binary_hdf5.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-export filepath=/net/scratch1/sli/trinity-run1/tracer
+export filepath=/net/scratch1/hli/trinity-run1
+export fpath_binary=$filepath/tracer
+export fpath_hdf5=$filepath/tracer_hdf5
 
 export particle=electron
-# tstep_max=28
-tstep_max=2996
+tstep_max=14
+# tstep_max=2996
 tstep_min=14
 tinterval=14
-ncpus=256
-echo "Maximum time step:" $tstep_max
+ncpus=1024
+# ncpus=32768
+
+# Create the directories
+if [ ! -d "$fpath_hdf5" ]; then
+    mkdir $fpath_hdf5
+fi
+i=$tstep_min
+while [ "$i" -le "$tstep_max" ]; do
+    if [ ! -d "$fpath_hdf5/T.$i" ]; then
+        mkdir $fpath_hdf5/T.$i
+    fi
+    i=$(($i+$tinterval))
+done
+
+echo "Minimum and maximum time step:" $tstep_min $tstep_max
 echo "Time interval:" $tinterval
+echo "Particle species:" $particle
+echo "Binary file path:" $filepath
+echo "Number of CPUs used in PIC simulation:" $ncpus
 
 mpirun -np 16 ./binary_to_hdf5 --tmin=$tstep_min --tmax=$tstep_max \
-    --tinterval=$tinterval --filepath=$filepath --species=${particle} \
-    --ncpus=$ncpus
+    --tinterval=$tinterval --fpath_binary=$fpath_binary \
+    --fpath_hdf5=$fpath_hdf5 --species=${particle} --ncpus=$ncpus
--- vpic-sorter/binary_to_hdf5.c
+++ vpic-sorter/binary_to_hdf5.c
@@ -14,25 +14,31 @@
 #include <math.h>
 #include <getopt.h>
 #include "constants.h"
-#include "vpic_data.h"
+#include "mpi_io.h"
 #include "get_data.h"
-#include "qsort-parallel.h"
-#include "particle_tags.h"
-#include "tracked_particle.h"
 
-int get_configuration(int argc, char **argv, int mpi_rank, char *filepath,
-        char *species, int *tmax, int *tmin, int *tinterval, int *ncpus);
+int get_configuration(int argc, char **argv, int mpi_rank, char *fpath_binary,
+        char *fpath_hdf5, char *species, int *tmax, int *tmin, int *tinterval,
+        int *ncpus);
 void print_help(void);
 void read_header(FILE *fp, int *nptl, float *q_m, double *t);
+dset_name_item *set_dname_array(int dataset_num);
+void set_jobs(int mpi_rank, int mpi_size, int njobs_tot, int *njobs, int *offset);
+void save_np_pic(int *np_local, char *fpath_hdf5, char *species, int ncpus,
+        int tstep);
+void get_package_data(int njobs, int offset, char *fpath_binary,
+        char *fname_binary, char *species, int tstep, int row_size,
+        char *package_data);
 
 /******************************************************************************
  * Main program to transfer the data
  ******************************************************************************/
 int main(int argc, char **argv)
 {
-    int mpi_size, mpi_rank, ncpus, is_help;
-    char *filepath, *species;
-    int tmax, tmin, tinterval;
+    int mpi_size, mpi_rank, ncpus;
+    char *fpath_binary, *fpath_hdf5, *species, *fname_binary, *fname_hdf5;
+    char *group_name;
+    int tmax, tmin, tinterval, tstep;
     double t0, t1;
 
     MPI_Comm comm = MPI_COMM_WORLD;
@@ -42,14 +48,93 @@ int main(int argc, char **argv)
 
     t0 = MPI_Wtime();
 
-    filepath = (char *)malloc(MAX_FILENAME_LEN * sizeof(char));
-    species = (char *)malloc(16 * sizeof(char));
+    fpath_binary = (char *)malloc(MAX_FILENAME_LEN * sizeof(char));
+    fpath_hdf5 = (char *)malloc(MAX_FILENAME_LEN * sizeof(char));
+    fname_binary = (char *)malloc(MAX_FILENAME_LEN * sizeof(char));
+    fname_hdf5 = (char *)malloc(MAX_FILENAME_LEN * sizeof(char));
+    species = (char *)malloc(32 * sizeof(char));
+    group_name = (char *)malloc(32 * sizeof(char));
 
-    get_configuration(argc, argv, mpi_rank, filepath,
+    get_configuration(argc, argv, mpi_rank, fpath_binary, fpath_hdf5,
         species, &tmax, &tmin, &tinterval, &ncpus);
 
+    /* The variables are required to use the sorting procedures for HDF5 */
+    dset_name_item *dname_array;
+    int max_type_size, dataset_num, row_size;
+    max_type_size = 4;
+    dataset_num = 13;
+    row_size = dataset_num * max_type_size;
+    dname_array = set_dname_array(dataset_num);
+
+    int nptl;
+    float q_m;
+    double t;
+    FILE *fp;
+    int *np_local; // number of particles in each PIC mpi_rank
+    int *np_all;   // for all PIC mpi_ranks
+    int njobs, offset;
+    long int nptl_tot;  // Total number of particles for current mpi_rank
+    
+    // Not actually necessary, but it is easies to use the existing code
+    int key_index, key_value_type;
+    key_index = 0;
+    key_value_type = H5GS_FLOAT32;
+
+    set_jobs(mpi_rank, mpi_size, ncpus, &njobs, &offset);
+    np_local = (int *)malloc(sizeof(int) * njobs);
+    np_all = (int *)malloc(sizeof(int) * ncpus);
+    for (tstep = tmin; tstep <= tmax; tstep += tinterval) {
+        for (int i = 0; i < njobs; i++) {
+            np_local[i] = 0;
+        }
+        for (int i = 0; i < ncpus; i++) {
+            np_all[i] = 0;
+        }
+        nptl_tot = 0;
+        // Get the particle number for each PIC mpi_rank
+        for (int icpu = offset; icpu < offset+njobs; icpu++) {
+            snprintf(fname_binary, MAX_FILENAME_LEN, "%s%s%d%s%s%s%d",
+                    fpath_binary, "/T.", tstep, "/", species, "_tracer.", icpu);
+            fp = fopen(fname_binary, "rb");
+            read_header(fp, &nptl, &q_m, &t);
+            nptl_tot += nptl;
+            np_local[icpu-offset] = nptl;
+            fclose(fp);
+        }
+        // Gather and save the particle number for each PIC mpi_rank
+        MPI_Gather(np_local, njobs, MPI_INT, np_all, njobs, MPI_INT, 0,
+                MPI_COMM_WORLD);
+        if (mpi_rank == 0) {
+            save_np_pic(np_local, fpath_hdf5, species, ncpus, tstep);
+        }
+        // Get the tracer data
+        char *package_data = (char *)malloc(row_size * nptl_tot);
+        get_package_data(njobs, offset, fpath_binary, fname_binary, species,
+                tstep, row_size, package_data);
+        hsize_t my_data_size = nptl_tot;
+
+        // Not actually necessary, but it is easies to use the existing code
+        set_variable_data(max_type_size, 0, dataset_num, key_value_type, 0);
+        snprintf(fname_hdf5, MAX_FILENAME_LEN, "%s%s%d%s%s%s",
+                fpath_hdf5, "/T.", tstep, "/", species, "_tracer.h5p");
+        snprintf(group_name, 32, "%s%d", "/Step#", tstep);
+
+        write_result_file(mpi_rank, mpi_size, package_data, my_data_size,
+                row_size, dataset_num, max_type_size, key_index, group_name,
+                fname_hdf5, "attribute", dname_array);
+        free(package_data);
+    }
+
+    free(np_all);
+    free(np_local);
+
+    free(dname_array);
     free(species);
-    free(filepath);
+    free(group_name);
+    free(fname_hdf5);
+    free(fname_binary);
+    free(fpath_hdf5);
+    free(fpath_binary);
 
     MPI_Barrier(comm);
     t1 = MPI_Wtime();
@@ -59,6 +144,63 @@ int main(int argc, char **argv)
 
     MPI_Finalize();
     return 0;
+}
+
+/******************************************************************************
+ * Save the number of particles in each PIC mpi_rank
+ ******************************************************************************/
+void save_np_pic(int *np_local, char *fpath_hdf5, char *species, int ncpus,
+        int tstep)
+{
+    char *fname_meta, *group_name;
+    hsize_t dimsf[1], count[1], offset[1];
+    hid_t file_id, group_id, dset_id, filespace, memspace;
+    int rank;
+    fname_meta = (char *)malloc(MAX_FILENAME_LEN * sizeof(char));
+    group_name = (char *)malloc(32 * sizeof(char));
+    snprintf(fname_meta, MAX_FILENAME_LEN, "%s%s%d%s%s%s",
+            fpath_hdf5, "/T.", tstep, "/grid_metadata_", 
+            species, "_tracer.h5p");
+    snprintf(group_name, 32, "%s%d", "/Step#", tstep);
+    file_id = H5Fcreate(fname_meta, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    group_id = H5Gcreate2(file_id, group_name, H5P_DEFAULT, H5P_DEFAULT,
+            H5P_DEFAULT);
+
+    rank = 1;
+    dimsf[0] = ncpus;
+    count[0] = ncpus;
+    offset[0] = 0;
+    filespace = H5Screate_simple(rank, dimsf, NULL);
+    memspace = H5Screate_simple(rank, count, NULL);
+    dset_id = H5Dcreate2(group_id, "np_local", H5T_NATIVE_INT, filespace,
+            H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
+    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace,
+            H5P_DEFAULT, np_local);
+    H5Sclose(filespace);
+    H5Sclose(memspace);
+    H5Dclose(dset_id);
+    H5Gclose(group_id);
+    H5Fclose(file_id);
+    free(group_name);
+    free(fname_meta);
+}
+
+/******************************************************************************
+ * Set number of jobs for each mpi_rank
+ ******************************************************************************/
+void set_jobs(int mpi_rank, int mpi_size, int njobs_tot, int *njobs, int *offset)
+{
+    int nleft;
+    *njobs = njobs_tot / mpi_size;
+    *offset = (*njobs) * mpi_rank;
+    nleft = njobs_tot % mpi_size;
+    if (mpi_rank < nleft) {
+        (*njobs) += 1;
+        (*offset) += mpi_rank;
+    } else {
+        (*offset) += nleft;
+    }
 }
 
 /******************************************************************************
@@ -77,23 +219,24 @@ void read_header(FILE *fp, int *nptl, float *q_m, double *t)
     fread(&tmpf, sizeof(float), 1, fp);
     fread(&tmpd, sizeof(double), 1, fp);
     /* printf("%d %f %f\n", *nptl, *q_m, *t); */
-    printf("%d\n", *nptl);
 }
 
 /******************************************************************************
  * Get the analysis configuration.
  ******************************************************************************/
-int get_configuration(int argc, char **argv, int mpi_rank, char *filepath,
-        char *species, int *tmax, int *tmin, int *tinterval, int *ncpus)
+int get_configuration(int argc, char **argv, int mpi_rank, char *fpath_binary,
+        char *fpath_hdf5, char *species, int *tmax, int *tmin, int *tinterval,
+        int *ncpus)
 {
     int c;
-    static const char *options="e:s:i:f:p:n:";
+    static const char *options="e:s:i:f:p:n:d:";
     static struct option long_options[] = 
     {
         {"tmax", required_argument, 0, 'e'},
         {"tmin", required_argument, 0, 's'},
         {"tinterval", required_argument, 0, 'i'},
-        {"filepath", required_argument, 0, 'f'},
+        {"fpath_binary", required_argument, 0, 'f'},
+        {"fpath_hdf5", required_argument, 0, 'd'},
         {"species", required_argument, 0, 'p'},
         {"ncpus", required_argument, 0, 'n'},
         {0, 0, 0, 0},
@@ -121,7 +264,10 @@ int get_configuration(int argc, char **argv, int mpi_rank, char *filepath,
                 *tinterval = atoi(optarg);
                 break;
             case 'f':
-                strcpy(filepath, optarg);
+                strcpy(fpath_binary, optarg);
+                break;
+            case 'd':
+                strcpy(fpath_hdf5, optarg);
                 break;
             case 'p':
                 strcpy(species, optarg);
@@ -153,7 +299,8 @@ void print_help(void)
                -s the particle output minimum time step \n\
                -i the particle output time interval \n\
                -n number of CPUs to use \n\
-               -f file path saving the particle tracing data \n\
+               -f file path of the binary data \n\
+               -f file path to save the HDF5 data \n\
                -p  particle species for sorting \n";
     fprintf(stdout, msg, "binary_to_hdf5");
 }
@@ -207,30 +354,12 @@ void set_filenames(char *filepath, int tstep, char *group_name,
 }
 
 /******************************************************************************
- * Get total number of particles for current MPI process.
+ * Get package data of all the data in the tracer files.
+ * We use char datatype for the package data
  ******************************************************************************/
-void get_nptl_tot(int mpi_rank, int mpi_size, int ncpus, char *filepath,
-        char *filename, int tstep, unsigned long long *nptl_tot)
-{
-    int nptl;
-    float q_m;
-    double t;
-    FILE *fp;
-    for (int i = mpi_rank; i < ncpus; i += mpi_size) {
-        snprintf(filename, MAX_FILENAME_LEN, "%s%s%d%s%d", filepath,
-                "/T.", tstep, "/electron_tracer.", i);
-        fp = fopen(filename, "rb");
-        read_header(fp, &nptl, &q_m, &t);
-        *nptl_tot += nptl;
-        fclose(fp);
-    }
-}
-
-/******************************************************************************
- * Get package data.
- ******************************************************************************/
-void get_package_data(int mpi_rank, int mpi_size, int ncpus, char *filepath,
-        char *filename, int tstep, int row_size, char *package_data)
+void get_package_data(int njobs, int offset, char *fpath_binary,
+        char *fname_binary, char *species, int tstep, int row_size,
+        char *package_data)
 {
     unsigned long long nptl_acc;
     int nptl;
@@ -240,10 +369,10 @@ void get_package_data(int mpi_rank, int mpi_size, int ncpus, char *filepath,
     char *data;
 
     nptl_acc = 0;
-    for (int i = mpi_rank; i < ncpus; i += mpi_size) {
-        snprintf(filename, MAX_FILENAME_LEN, "%s%s%d%s%d", filepath,
-                "/T.", tstep, "/electron_tracer.", i);
-        fp = fopen(filename, "rb");
+    for (int i = offset; i < offset+njobs; i++) {
+        snprintf(fname_binary, MAX_FILENAME_LEN, "%s%s%d%s%s%s%d",
+                fpath_binary, "/T.", tstep, "/", species, "_tracer.", i);
+        fp = fopen(fname_binary, "rb");
         read_header(fp, &nptl, &q_m, &t);
         data = (char *)malloc(row_size*nptl*sizeof(char));
         fread(data, sizeof(char), row_size*nptl, fp);
