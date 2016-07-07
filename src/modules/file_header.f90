356ad1640cfffea6ab1b46396d153e36567e9cfc
Xiaocan Li <phyxiaolee@gmail.com>
2016-07-06 18:19:55 -0600

    New subroutine to set file header

--- src/modules/file_header.f90
+++ src/modules/file_header.f90
@@ -5,7 +5,7 @@ module file_header
     implicit none
     private
     public v0, pheader, fheader, read_boilerplate, read_fields_header, &
-           read_particle_header
+           read_particle_header, set_v0header
     integer :: itype, ndim
     ! Define structure for V0 header
     type v0header
@@ -72,5 +72,23 @@ module file_header
         read(fh) v0
         read(fh) pheader
     end subroutine read_particle_header
+
+    !<--------------------------------------------------------------------------
+    !< Set v0 header. Currently, it includes only spatial information.
+    !<--------------------------------------------------------------------------
+    subroutine set_v0header(nx, ny, nz, x0, y0, z0, dx, dy, dz)
+        implicit none
+        integer(kind=4), intent(in) :: nx, ny, nz
+        real(kind=4), intent(in) :: x0, y0, z0, dx, dy, dz
+        v0%nx = nx
+        v0%ny = ny
+        v0%nz = nz
+        v0%x0 = x0
+        v0%y0 = y0
+        v0%z0 = z0
+        v0%dx = dx
+        v0%dy = dy
+        v0%dz = dz
+    end subroutine set_v0header
 
 end module file_header
