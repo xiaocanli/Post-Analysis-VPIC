3bba76754b94c5f35d97ee1614a71046f4f7c372
Xiaocan Li <phyxiaolee@gmail.com>
2016-09-20 08:36:17 -0600

    Update plots for particle number density

--- python/contour_plots.py
+++ python/contour_plots.py
@@ -22,6 +22,9 @@ import color_maps as cm
 import colormap.colormaps as cmaps
 from runs_name_path import ApJ_long_paper_runs
 from energy_conversion import read_data_from_json
+from shell_functions import mkdir_p
+from joblib import Parallel, delayed
+import multiprocessing
 
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 mpl.rc('text', usetex=True)
@@ -475,52 +478,75 @@ def plot_by(pic_info):
     plt.show()
     # plt.close()
 
-def plot_number_density(pic_info, species, current_time):
+def plot_number_density(pic_info, species, ct, run_name, base_dir='../../'):
     """Plot plasma beta and number density.
 
     Args:
         pic_info: namedtuple for the PIC simulation information.
         species: 'e' for electrons, 'i' for ions.
-        current_time: current time frame.
+        ct current time frame.
     """
-    kwargs = {"current_time":current_time, "xl":0, "xr":200, "zb":-50, "zt":50}
-    x, z, num_rho = read_2d_fields(pic_info, "../data/ne.gda", **kwargs) 
-    x, z, Ay = read_2d_fields(pic_info, "../data/Ay.gda", **kwargs) 
+    xmin, xmax = 0, pic_info.lx_di
+    xmax = 105
+    zmin, zmax = -0.5*pic_info.lz_di, 0.5*pic_info.lz_di
+    kwargs = {"current_time":ct, "xl":xmin, "xr":xmax, "zb":zmin, "zt":zmax}
+    fname = base_dir + 'data/n' + species + '.gda'
+    x, z, num_rho = read_2d_fields(pic_info, fname, **kwargs) 
+    # fname = base_dir + 'data/Ay.gda'
+    # x, z, Ay = read_2d_fields(pic_info, fname, **kwargs) 
     nx, = x.shape
     nz, = z.shape
-    width = 0.8
-    height = 0.78
-    xs = 0.10
-    ys = 0.9 - height
-    fig = plt.figure(figsize=[16,8])
-    ax1 = fig.add_axes([xs, ys, width, height])
+    nrho_cum = np.sum(num_rho, axis=0) / nz
+    nrho_grad = np.abs(np.gradient(nrho_cum))
+    xs = 5
+    max_index = np.argmax(nrho_grad[xs:])
+    xm = x[max_index]
+    print xm, max_index
+
+    w1, h1 = 0.7, 0.52
+    xs, ys = 0.15, 0.94 - h1
+    gap = 0.05
+
+    width, height = 10, 12
+    fig = plt.figure(figsize=[10,12])
+    ax1 = fig.add_axes([xs, ys, w1, h1])
     kwargs_plot = {"xstep":2, "zstep":2, "is_log":True, "vmin":0.1, "vmax":10}
     xstep = kwargs_plot["xstep"]
     zstep = kwargs_plot["zstep"]
     p1, cbar1 = plot_2d_contour(x, z, num_rho, ax1, fig, **kwargs_plot)
-    p1.set_cmap(plt.cm.seismic)
-    ax1.contour(x[0:nx:xstep], z[0:nz:zstep], Ay[0:nz:zstep, 0:nx:xstep], 
-            colors='black', linewidths=0.5)
-    ax1.set_ylabel(r'$z/d_i$', fontdict=font, fontsize=32)
-    ax1.set_xlabel(r'$x/d_i$', fontdict=font, fontsize=32)
-    ax1.tick_params(labelsize=32)
-    cbar1.ax.set_ylabel(r'$n_e$', fontdict=font, fontsize=32)
-    cbar1.ax.tick_params(labelsize=32)
-    
-    t_wci = current_time*pic_info.dt_fields
-    title = r'$t = ' + "{:10.1f}".format(t_wci) + '/\Omega_{ci}$'
-    ax1.set_title(title, fontdict=font, fontsize=32)
+    p1.set_cmap(plt.cm.jet)
+    # ax1.contour(x[0:nx:xstep], z[0:nz:zstep], Ay[0:nz:zstep, 0:nx:xstep], 
+    #         colors='black', linewidths=0.5)
+    ax1.set_xlim([xmin, xmax])
+    ax1.set_ylabel(r'$z/d_i$', fontdict=font, fontsize=24)
+    ax1.tick_params(labelsize=24)
+    lname = r'$n_' + species + '$'
+    cbar1.ax.set_ylabel(lname, fontdict=font, fontsize=24)
+    cbar1.ax.tick_params(labelsize=24)
 
-    if not os.path.isdir('../img/'):
-        os.makedirs('../img/')
-    if not os.path.isdir('../img/img_num_rho/'):
-        os.makedirs('../img/img_num_rho/')
-    fname = '../img/img_num_rho/num_rho_' + species + '_' + \
-            str(current_time).zfill(3) + '.jpg'
+    ax1.plot([xm, xm], [zmin, zmax], color='k', linestyle='--')
+    
+    t_wci = ct*pic_info.dt_fields
+    title = r'$t = ' + "{:10.1f}".format(t_wci) + '/\Omega_{ci}$'
+    ax1.set_title(title, fontdict=font, fontsize=24)
+
+    h2 = 0.3
+    ys -= gap + h2
+    w2 = w1 * 0.98 - 0.05 / width
+    ax2 = fig.add_axes([xs, ys, w2, h2])
+    ax2.plot(x, nrho_cum, linewidth=2, color='k')
+    ax2.plot([xm, xm], ax2.get_ylim(), color='k', linestyle='--')
+    ax2.set_xlim([xmin, xmax])
+    ax2.tick_params(labelsize=24)
+    ax2.set_xlabel(r'$x/d_i$', fontdict=font, fontsize=24)
+
+    fig_dir = '../img/img_number_densities/' + run_name + '/'
+    mkdir_p(fig_dir)
+    fname = fig_dir + '/nrho_' + species + '_' + str(ct).zfill(3) + '.jpg'
     fig.savefig(fname)
 
-    plt.show()
-    #plt.close()
+    # plt.show()
+    plt.close()
 
 
 def get_anisotropy_data(pic_info, species, ct, rootpath='../../'):
@@ -1326,7 +1352,7 @@ if __name__ == "__main__":
     # plot_beta_rho(pic_info)
     # plot_jdote_2d(pic_info)
     # plot_anistropy(pic_info, 'e')
-    plot_phi_parallel(29, pic_info)
+    # plot_phi_parallel(29, pic_info)
     # maps = sorted(m for m in plt.cm.datad if not m.endswith("_r"))
     # nmaps = len(maps) + 1
     # print nmaps
@@ -1340,7 +1366,7 @@ if __name__ == "__main__":
     # for i in range(90, 170, 10):
     #     plot_absB_jy(pic_info, 'e', i)
     # plot_by(pic_info)
-    plot_by_multi()
+    # plot_by_multi()
     # plot_ux(pic_info, 'e', 160)
     # plot_uy(pic_info, 160)
     # plot_diff_fields(pic_info, 'e', 120)
@@ -1355,3 +1381,14 @@ if __name__ == "__main__":
     # plot_jpolar_dote(pic_info, 'e', 30)
     # plot_epara(pic_info, 'e', 35)
     # plot_anisotropy_multi('e')
+    base_dir = '/net/scratch3/xiaocanli/2D-90-Mach4-sheet4-multi/'
+    run_name = '2D-90-Mach4-sheet4-multi'
+    picinfo_fname = '../data/pic_info/pic_info_' + run_name + '.json'
+    pic_info = read_data_from_json(picinfo_fname)
+    ct = pic_info.ntf - 2
+    cts = range(pic_info.ntf - 1)
+    def processInput(ct):
+        print ct
+        plot_number_density(pic_info, 'i', ct, run_name, base_dir)
+    num_cores = multiprocessing.cpu_count()
+    Parallel(n_jobs=num_cores)(delayed(processInput)(ct) for ct in cts)
