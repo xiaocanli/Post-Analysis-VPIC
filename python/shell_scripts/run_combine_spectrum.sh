66e8f7e696ff87e83c13da6b23f01f183be59d80
Xiaocan Li <phyxiaolee@gmail.com>
2018-06-07 14:27:04 -0600

    Update the script when run names are different
    The simulation has a name same as its run directory, while we could
    rename it when doing analysis.

--- python/shell_scripts/run_combine_spectrum.sh
+++ python/shell_scripts/run_combine_spectrum.sh
@@ -1,13 +1,18 @@
-root_path=/net/scratch3/xiaocanli/reconnection/mime100/
+root_path=/net/scratch3/xiaocanli/reconnection/mime25/
+cd ../
 
-run_name=mime100_beta002_bg00
-python combine_energy_spectrum.py --run_dir $root_path/$run_name/ --run_name $run_name --multi_frames
+pic_run=mime25_beta002_bg00
+run_name=mime25_beta002_bg00_lx100
+python combine_energy_spectrum.py --run_dir $root_path/$pic_run/ --run_name $run_name --multi_frames
 
-run_name=mime100_beta002_bg02
-python combine_energy_spectrum.py --run_dir $root_path/$run_name/ --run_name $run_name --multi_frames
+pic_run=mime25_beta002_bg02
+run_name=mime25_beta002_bg02_lx100
+python combine_energy_spectrum.py --run_dir $root_path/$pic_run/ --run_name $run_name --multi_frames
 
-run_name=mime100_beta002_bg04
-python combine_energy_spectrum.py --run_dir $root_path/$run_name/ --run_name $run_name --multi_frames
+pic_run=mime25_beta002_bg04
+run_name=mime25_beta002_bg04_lx100
+python combine_energy_spectrum.py --run_dir $root_path/$pic_run/ --run_name $run_name --multi_frames
 
-run_name=mime100_beta002_bg08
-python combine_energy_spectrum.py --run_dir $root_path/$run_name/ --run_name $run_name --multi_frames
+pic_run=mime25_beta002_bg08
+run_name=mime25_beta002_bg08_lx100
+python combine_energy_spectrum.py --run_dir $root_path/$pic_run/ --run_name $run_name --multi_frames
