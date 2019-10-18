73b46084fc3e18e680230e11bc462889df949958
Xiaocan Li <phyxiaolee@gmail.com>
2019-10-18 08:22:26 -0600

    Update script for combining spectrum in all zones
    It works for different particle species, including test particles

--- config_files/combine_spectrum_h5.sh
+++ config_files/combine_spectrum_h5.sh
@@ -16,71 +16,67 @@ ch_htt () {
     sed -i -e "s/\(httz = \).*,\(.*\)/\1$3,\2/" $conf
 }
 
-mpi_size=128
-node_size=4
+mpi_size=64
+node_size=2
+
+combine_spectrum_single_species () {
+    cd $ana_path
+    srun -n $mpi_size ./combine_spectrum_hdf5.exec \
+         -rp $1 -sp $2 --pic_mpi_size $3 --nzones $4 \
+         --ndata $5 -ts $6 -te $7 -ti $8
+}
 
 combine_spectrum () {
     cd $ana_path
-    srun -n $mpi_size ./combine_spectrum_hdf5.exec \
-         -rp $1 --pic_mpi_size $2 --nzones $3 --ndata $4 -ts $5 -te $6 -ti $7
+    for sp in electron ion
+    do
+        combine_spectrum_single_species $1 $sp $2 $3 $4 $5 $6 $7
+    done
 }
 
-runs_path=/net/scratch4/xiaocanli/reconnection/mime25_high/
-run_name=mime25_beta002_bg00_high
+combine_spectrum_multi_species () {
+    cd $ana_path
+    for sp in electron ion
+    do
+        echo $sp
+        combine_spectrum_single_species $1 $sp $2 $3 $4 $5 $6 $7
+    done
+
+    for sp in electron_wo_epara ion_wo_epara electron_wo_eparay ion_wo_eparay
+    do
+        echo $sp
+        combine_spectrum_single_species $1 $sp $2 $3 $4 $5 $6 $7
+    done
+
+    for sp in electron_wo_egtb ion_wo_egtb electron_egtb ion_egtb
+    do
+        echo $sp
+        combine_spectrum_single_species $1 $sp $2 $3 $4 $5 $6 $7
+    done
+
+    for sp in electron_egtb_egtb ion_egtb_egtb
+    do
+        echo $sp
+        combine_spectrum_single_species $1 $sp $2 $3 $4 $5 $6 $7
+    done
+
+    # for sp in electron_wo_eperp ion_wo_eperp
+    # do
+    #     echo $sp
+    #     combine_spectrum_single_species $1 $sp $2 $3 $4 $5 $6 $7
+    # done
+}
+
+runs_path=/net/scratch4/xiaocanli/reconnection/power_law_index/
+run_name=high_sigma_test
 rootpath=$runs_path/$run_name/
-pic_mpi_size=4096
-nzones=1
+pic_mpi_size=2048
+nzones=256
 ndata=1003
 tstart=0
-tend=374030
-tinterval=3310
+tend=32960
+tinterval=206
 # combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
 #                  $tstart $tend $tinterval
-
-run_name=mime25_beta002_bg02_high
-rootpath=$runs_path/$run_name/
-tend=374030
-# combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
-#                  $tstart $tend $tinterval
-
-run_name=mime25_beta002_bg04_high
-rootpath=$runs_path/$run_name/
-tend=486570
-combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
-                 $tstart $tend $tinterval
-
-run_name=mime25_beta002_bg08_high
-rootpath=$runs_path/$run_name/
-tend=403820
-combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
-                 $tstart $tend $tinterval
-
-runs_path=/net/scratch4/xiaocanli/reconnection/mime100_high/
-run_name=mime100_beta002_bg00_high
-rootpath=$runs_path/$run_name/
-pic_mpi_size=4096
-nzones=1
-ndata=1003
-tstart=0
-tend=387270
-tinterval=3310
-combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
-                 $tstart $tend $tinterval
-
-run_name=mime100_beta002_bg02_high
-rootpath=$runs_path/$run_name/
-tend=367410
-combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
-                 $tstart $tend $tinterval
-
-run_name=mime100_beta002_bg04_high
-rootpath=$runs_path/$run_name/
-tend=374030
-combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
-                 $tstart $tend $tinterval
-
-run_name=mime100_beta002_bg08_high
-rootpath=$runs_path/$run_name/
-tend=390580
-# combine_spectrum $rootpath $pic_mpi_size $nzones $ndata \
-#                  $tstart $tend $tinterval
+# combine_spectrum_multi_species $rootpath $pic_mpi_size $nzones $ndata \
+#                                $tstart $tend $tinterval
