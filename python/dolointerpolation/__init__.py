b502a3de5b98290b8d3efb1a62e957e2bbe467ea
Xiaocan Li <4249978+xiaocanli@users.noreply.github.com>
2021-04-05 10:47:11 -0700

    Archive old analysis scripts

--- python/dolointerpolation/__init__.py
+++
@@ -1,4 +0,0 @@
-'''Multilinear interpolation routine by Pablo Winant
-'''
-from .multilinear import MultilinearInterpolator
-from .multilinear_cython import multilinear_interpolation
--- python/dolointerpolation/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    name = 'multilinear_cython',
-    ext_modules = cythonize("multilinear_cython.pyx"),
-    )
